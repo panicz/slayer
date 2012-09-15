da7f797f0517e7d5be81f3e093cb01f2661cf7d8
Maciek Godek <godek.maciek@gmail.com>
2012-09-16 01:02:06 +0200

    The function sdl_color has been tested and seems to work fine on x86

--- slayer.h
+++ slayer.h
@@ -33,9 +33,9 @@ static inline SDL_Color sdl_color(Uint32 rgba) {
 #if SDL_BYTEORDER == SDL_BIG_ENDIAN
   *((Uint32 *) &c) = rgba;  
 #else
-  c.r = 0xff & (rgba);
+  c.b = 0xff & (rgba);
   c.g = 0xff & (rgba >> 8);
-  c.b = 0xff & (rgba >> 16);
+  c.r = 0xff & (rgba >> 16);
   c.unused = 0xff & (rgba >> 24);
 #endif
   return c;
