24b1afc9bb6f2d939cb5d04abf0074103385f96d
Maciek Godek <godek.maciek@gmail.com>
2012-10-16 14:21:56 +0200

    The meaning of the LOG macro has changed. It now accepts only one argument, an expression. The expression is then executed, and afterwards it is printed to stderr, along with the information of file, line and function. The previous behaviour of the LOG macro is now carried out by the newly created OUT macro

--- utils.h
+++ utils.h
@@ -3,10 +3,13 @@
 #include <stdio.h>
 #include <assert.h>
 #include <stdlib.h>
-#define LOG(msg, ...) fprintf(stderr, msg "\n", ## __VA_ARGS__ )
+
+#define TOSTRING(x) # x
+#define LOG(expr) fprintf(stderr, "%s/%s(%d): " TOSTRING(expr) "\n", __FILE__, __FUNCTION__, __LINE__); expr
+#define OUT(msg, ...) fprintf(stderr, msg "\n", ## __VA_ARGS__ )
 #define FATAL(msg, ...) do { fprintf(stderr, "FATAL ERROR: " msg "\n", ## __VA_ARGS__ ); exit(0); } while(0)
 #define NELEMS(array) (sizeof(array)/sizeof array[0])
-#define WARN(msg, ...) fprintf(stderr, "%s/\%s: " msg "\n", __FILE__, __FUNCTION__, ## __VA_ARGS__ )
+#define WARN(msg, ...) fprintf(stderr, "%s/%s: " msg "\n", __FILE__, __FUNCTION__, ## __VA_ARGS__ )
 //do { } while(0)
 
 
