33848222f7bf1f31bbdbb8b16c75affa989f757e
Maciek Godek <godek.maciek@gmail.com>
2012-10-19 19:00:09 +0200

    Current time is now displayed with LOGs and WARNings (from SDL_GetTicks, and hence after video_init)

--- utils.h
+++ utils.h
@@ -3,17 +3,25 @@
 #include <stdio.h>
 #include <assert.h>
 #include <stdlib.h>
+#include <time.h>
+#include <SDL.h>
 
-#define TOSTRING(x) # x
-#define LOG(expr) fprintf(stderr, "%s/%s(%d): " TOSTRING(expr) "\n", __FILE__, __FUNCTION__, __LINE__); expr
+#define now() SDL_GetTicks()
 #define OUT(msg, ...) fprintf(stderr, msg "\n", ## __VA_ARGS__ )
+#define OUT_(msg, ...) fprintf(stderr, msg, ## __VA_ARGS__ )
+#define STR(x) # x
+#define LOG(expr) ({OUT("[ %09d ] %s/%s[%d]: "STR(expr),now(),__FILE__,__FUNCTION__,__LINE__);expr;})
+#define LOGTIME(expr) \
+  ({int time=now();expr;time=now()-time;\
+    OUT("[ %09d ] %s/%s[%d]: "STR(expr)" (%d ticks)",now(),__FILE__,__FUNCTION__,__LINE__,time);})
+
 #define FATAL(msg, ...) do { fprintf(stderr, "FATAL ERROR: " msg "\n", ## __VA_ARGS__ ); exit(0); } while(0)
 #define NELEMS(array) (sizeof(array)/sizeof array[0])
-#define WARN(msg, ...) fprintf(stderr, "%s/%s: " msg "\n", __FILE__, __FUNCTION__, ## __VA_ARGS__ )
+#define WARN(msg, ...) fprintf(stderr, "[ %09d ] %s/%s: " msg "\n", now(), __FILE__, __FUNCTION__, ## __VA_ARGS__ )
 #define WARN_UPTO(n, msg, ...) \
   ({ void __fn__(const char *f) { \
       static int c=0; \
-      if(c++ < n) OUT("%s/%s: " msg " (warning %i of %i)", __FILE__, f, ## __VA_ARGS__, c, n); \
+      if(c++ < n) OUT("[ %09d ] %s/%s: " msg " (warning %i of %i)", now(), __FILE__, f, ## __VA_ARGS__, c, n); \
     } __fn__(__FUNCTION__); })
 #define WARN_ONCE(msg, ...) WARN_UPTO(1, msg, ## __VA_ARGS__ )
 
