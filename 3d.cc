0f2475a896384b2f1addb9c394bcb601f7b1e485
Maciek Godek <godek.maciek@gmail.com>
2012-11-01 11:53:50 +0100

    Added support for home-brewed scheme-based file format for storing 3d mesh. The implementation is really straightforward and one might hope it will be extended in the future. The file 3d/cube.3d is added as an example, and it is used as the default mesh by <3d-mesh> object.

--- 3d.cc
+++ 3d.cc
@@ -5,39 +5,46 @@
 
 extern "C" void glWindowPos2i (GLint x, GLint y);
 
-#define DEF_SCM_VECTOR_TO_V(n,t,ype)					\
-static inline v##n##t							\
-scm_to_v##n##t(SCM vector) {						\
-  int i; v##n##t v;							\
-  if (scm_is_uniform_vector(vector))					\
-    for (i = 0; i < n; ++i)						\
+#define DEF_SCM_TO_V(n,t,ype) \
+static inline v##n##t \
+scm_to_v##n##t(SCM vector) { \
+  int i; v##n##t v; \
+  if (scm_is_uniform_vector(vector)) \
+    for (i = 0; i < n; ++i) \
       v[i] = scm_to_##t##ype(scm_uniform_vector_ref(vector, scm_from_int(i))); \
-  else if (scm_is_vector(vector))					\
-    for (i = 0; i < n; ++i)						\
-      v[i] = scm_to_##t##ype(scm_c_vector_ref(vector, i));		\
-  else									\
-    WARN("function called on a non-vector");				\
-  return v;								\
+  else if (scm_is_vector(vector)) \
+    for (i = 0; i < n; ++i) \
+      v[i] = scm_to_##t##ype(scm_c_vector_ref(vector, i)); \
+  else \
+    WARN("function called on a non-vector"); \
+  return v; \
 }
 
-DEF_SCM_VECTOR_TO_V(2,f,loat);
-DEF_SCM_VECTOR_TO_V(2,d,ouble);
-DEF_SCM_VECTOR_TO_V(3,f,loat);
-DEF_SCM_VECTOR_TO_V(3,d,ouble);
-DEF_SCM_VECTOR_TO_V(4,f,loat);
-DEF_SCM_VECTOR_TO_V(4,d,ouble);
+DEF_SCM_TO_V(2,f,loat);
+DEF_SCM_TO_V(2,d,ouble);
+DEF_SCM_TO_V(3,f,loat);
+DEF_SCM_TO_V(3,d,ouble);
+DEF_SCM_TO_V(4,f,loat);
+DEF_SCM_TO_V(4,d,ouble);
 
-#undef DEF_SCM_VECTOR_TO_V
+#undef DEF_SCM_TO_V
 
 static inline qtf
-scm_to_qtf(SCM vector) {
-  return qtf(scm_to_v4f(vector));
+scm_to_qtf(SCM pair) {
+  qtf q;
+  q.s = scm_to_float(SCM_CAR(pair));
+  q.v = scm_to_v3f(SCM_CDR(pair));
+  return q;
 }
 
 static inline qtd
-scm_to_qtd(SCM vector) {
-  return qtd(scm_to_v4d(vector));
+scm_to_qtd(SCM pair) {
+  qtd q;
+  q.s = scm_to_double(SCM_CAR(pair));
+  q.v = scm_to_v3d(SCM_CDR(pair));
+  return q;
 }
+
 
 static SCM
 push_matrix_x() {
@@ -66,9 +73,14 @@ rotate_view_x(SCM quaternion) {
 }
 
 static SCM
-set_viewport_x(SCM x, SCM y, SCM w, SCM h) {
-  glViewport((GLint) scm_to_int(x), (GLint) screen->h - scm_to_int(y) - scm_to_int(h), 
-	     (GLsizei) scm_to_int(w), (GLsizei) scm_to_int(h));
+set_viewport_x(SCM X, SCM Y, SCM W, SCM H) {
+  GLsizei w = scm_to_int(W);
+  GLsizei h = scm_to_int(H);
+
+  GLint x = scm_to_int(X);
+  GLint y = screen->h - scm_to_int(Y) - h;
+
+  glViewport(x, y, w, h);
   return SCM_UNSPECIFIED;
 }
 
@@ -81,61 +93,224 @@ current_viewport() {
 }
 
 static SCM
-draw_mesh(SCM mesh) {
-  glColor3f(1.0, 0.7, 0.3);
-  glBegin(GL_TRIANGLES);
+perspective_projection_x(SCM FOVY, SCM ASPECT, SCM NEAR, SCM FAR) {
+  GLdouble fovy = scm_to_float(FOVY);
+  GLdouble aspect = ((ASPECT == SCM_UNDEFINED)
+		     || (ASPECT == SCM_UNSPECIFIED)
+		     || (ASPECT == SCM_BOOL_F))
+    ? (GLdouble) screen->w / (GLdouble) screen->h
+    : (GLdouble) scm_to_double(ASPECT);
+  GLdouble near = (NEAR == SCM_UNDEFINED)
+    ? 0.01 : (GLdouble) scm_to_double(NEAR);
+  GLdouble far = (FAR == SCM_UNDEFINED)
+    ? 1000.0 : (GLdouble) scm_to_double(FAR);
 
-  glVertex3i(0, 0, 0);
-  glVertex3i(0, 1, 0);
-  glVertex3i(1, 0, 0);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  gluPerspective(fovy, aspect, near, far);
 
-  glVertex3i(0, 0, 0);
-  glVertex3i(0, -1, 0);
-  glVertex3i(-1, 0, 0);
-
-  glColor3f(0.7, 1.0, 0.3);
-  glVertex3i(0, 0, 0);
-  glVertex3i(0, -2, 0);
-  glVertex3i(2, 0, 0);
-
-  glEnd();
-  glFinish();
+  glMatrixMode(GL_MODELVIEW);
+  // by convention, each function that ever
+  // alters a matrix mode, should leave GL_MODELVIEW
+  // as the current matrix mode.
+    
   return SCM_UNSPECIFIED;
+}
+
+static SCM
+ortographic_projection_x() {
+  WARN("unimplemented");
+  return SCM_UNSPECIFIED;
+}
+
+
+static GLenum GLtypes[SCM_ARRAY_ELEMENT_TYPE_LAST+1];
+static inline void
+init_GLtypes() {
+
+#define NOT_SUPPORTED -1
+  unsigned int i;
+  for(i = 0; i < NELEMS(GLtypes); ++i)
+    GLtypes[i] = NOT_SUPPORTED;
+
+  /* this is rather unlikely, but it never hurts to check */
+#if  (GL_FLOAT == NOT_SUPPORTED)		\
+  or (GL_DOUBLE == NOT_SUPPORTED)		\
+  or (GL_INT == NOT_SUPPORTED)			\
+  or (GL_BYTE == NOT_SUPPORTED)			\
+  or (GL_UNSIGNED_BYTE == NOT_SUPPORTED)	\
+  or (GL_UNSIGNED_INT == NOT_SUPPORTED)		\
+  or (GL_SHORT_INT == NOT_SUPPORTED)	
+#error "Invalid special value (the ``NOT_SUPPORTED'' macro constant"
+#endif
+
+#define SET_TYPE(type, GLtype) \
+  GLtypes[SCM_ARRAY_ELEMENT_TYPE_##type] = GLtype
+
+  SET_TYPE(S8, GL_BYTE);
+  SET_TYPE(U8, GL_UNSIGNED_BYTE);
+
+  SET_TYPE(S16, GL_SHORT);
+  SET_TYPE(U16, GL_UNSIGNED_SHORT);
+  
+  SET_TYPE(S32, GL_INT);
+  SET_TYPE(U32, GL_UNSIGNED_INT);
+
+  SET_TYPE(F32, GL_FLOAT);
+  SET_TYPE(F64, GL_DOUBLE);
+
+#undef SET_TYPE
+}
+
+#define DEF_SET_GL_ARRAY(objects, proc, cls)	\
+static SCM \
+set_##objects##_array_x(SCM array) { \
+  glEnableClientState(cls); \
+  if (!scm_is_array(array)) { \
+    WARN("argument is not an array"); \
+    return SCM_UNSPECIFIED; \
+  } \
+  scm_t_array_handle handle; \
+  scm_t_array_dim *dims; \
+  const void *data; \
+  GLint size; \
+  scm_array_get_handle(array, &handle); \
+  int type = GLtypes[handle.element_type]; \
+  if (type == NOT_SUPPORTED) { \
+    WARN("unsupported array type"); \
+    goto end; \
+  } \
+  if (scm_array_handle_rank(&handle) != 2) { \
+    WARN("invalid array dimension"); \
+    goto end; \
+  } \
+  dims = scm_array_handle_dims(&handle); \
+  size = abs(dims[1].ubnd - dims[1].lbnd) + 1; \
+  data = scm_array_handle_uniform_elements(&handle); \
+  proc(size, (GLenum) type, 0, data); \
+ end: \
+  scm_array_handle_release(&handle); \
+  return SCM_UNSPECIFIED; \
+}
+
+DEF_SET_GL_ARRAY(vertices, glVertexPointer, GL_VERTEX_ARRAY);
+DEF_SET_GL_ARRAY(colors, glColorPointer, GL_COLOR_ARRAY);
+DEF_SET_GL_ARRAY(texture_coords, glTexCoordPointer, 
+		 GL_TEXTURE_COORD_ARRAY);
+
+#undef DEF_SET_GL_ARRAY
+#undef NOT_SUPPORTED
+
+static SCM GLnames;
+static inline void
+init_GLnames() {
+  GLnames = scm_c_make_hash_table(64);
+  hold_scm(GLnames);
+
+#define SET_VALUE(name, value) \
+  scm_hash_set_x(GLnames, symbol(name), scm_from_int(value))
+
+  SET_VALUE("points", GL_POINTS);
+  SET_VALUE("lines", GL_LINES);
+  SET_VALUE("line-strip", GL_LINE_STRIP);
+  SET_VALUE("line-loop", GL_LINE_LOOP);
+  SET_VALUE("triangles", GL_TRIANGLES);
+  SET_VALUE("triangle-strip", GL_TRIANGLE_STRIP);
+  SET_VALUE("triangle-fan", GL_TRIANGLE_FAN);
+  SET_VALUE("quads", GL_QUADS);
+  SET_VALUE("quad-strip", GL_QUAD_STRIP);
+  SET_VALUE("polygon", GL_QUAD_STRIP);
+  
+#undef SET_VALUE
+}
+
+
+
+
+static SCM
+draw_faces_x(SCM type, SCM array) {
+#define GET_VALUE(name) \
+  scm_to_int(scm_hash_ref(GLnames, name, SCM_BOOL_F))
+
+  if (!scm_is_array(array)) {
+    WARN("argument is not an array");
+    return SCM_UNSPECIFIED;
+  }
+
+  scm_t_array_handle handle;
+  scm_array_get_handle(array, &handle);
+
+  size_t nelems = scm_array_handle_nelems(&handle);
+  
+  char *name = as_c_string(type);
+  //WARN("drawing %s (%d) with %d vertices", name, GET_VALUE(type), nelems);
+  free(name);
+  const void *data 
+    = scm_array_handle_uniform_writable_elements(&handle);
+
+  glDrawElements(GET_VALUE(type), nelems, 
+		 GLtypes[handle.element_type], data);
+
+
+  //OUT("glDrawElements(%d, %d, %d, %p", GET_VALUE(type), nelems, GLtypes[handle.element_type], data);
+
+  scm_array_handle_release(&handle);
+  return SCM_UNSPECIFIED;
+#undef GET_VALUE
 }
 
 static void
 export_symbols() {
-  scm_c_define_gsubr("push-matrix!", 0, 0, 0, (scm_t_subr) push_matrix_x);
-  scm_c_define_gsubr("pop-matrix!", 0, 0, 0, (scm_t_subr) pop_matrix_x);
-  scm_c_define_gsubr("translate-view!", 1, 0, 0, (scm_t_subr) translate_view_x);
-  scm_c_define_gsubr("rotate-view!", 1, 0, 0, (scm_t_subr) rotate_view_x);
-  scm_c_define_gsubr("set-viewport!", 4, 0, 0, (scm_t_subr) set_viewport_x);
-  scm_c_define_gsubr("current-viewport", 0, 0, 0, (scm_t_subr) current_viewport);
-  scm_c_define_gsubr("draw-mesh", 1, 0, 0, (scm_t_subr) draw_mesh);
+  scm_c_define_gsubr("push-matrix!", 0, 0, 0, 
+		     (scm_t_subr) push_matrix_x);
+  scm_c_define_gsubr("pop-matrix!", 0, 0, 0, 
+		     (scm_t_subr) pop_matrix_x);
+  scm_c_define_gsubr("translate-view!", 1, 0, 0, 
+		     (scm_t_subr) translate_view_x);
+  scm_c_define_gsubr("rotate-view!", 1, 0, 0, 
+		     (scm_t_subr) rotate_view_x);
+  scm_c_define_gsubr("set-viewport!", 4, 0, 0, 
+		     (scm_t_subr) set_viewport_x);
+  scm_c_define_gsubr("current-viewport", 0, 0, 0, 
+		     (scm_t_subr) current_viewport);
+  scm_c_define_gsubr("perspective-projection!", 1, 3, 0, 
+		     (scm_t_subr) perspective_projection_x);
+  scm_c_define_gsubr("ortographic-projection!", 4, 2, 0, 
+		     (scm_t_subr) ortographic_projection_x);
+  scm_c_define_gsubr("set-vertices-array!", 1, 0, 0, 
+		     (scm_t_subr) set_vertices_array_x);
+  scm_c_define_gsubr("set-colors-array!", 1, 0, 0, 
+		     (scm_t_subr) set_colors_array_x);
+  scm_c_define_gsubr("draw-faces!", 2, 0, 0, 
+		     (scm_t_subr) draw_faces_x);
+
 }
 
 extern "C" void
 init_3d(Uint16 w, Uint16 h) {
+  init_GLtypes();
+  init_GLnames();
+
   SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-  glEnable(GL_DEPTH_TEST);
+  //glEnable(GL_DEPTH_TEST);
   glClearDepth(1.0);
   glDepthMask(GL_TRUE);
+
   glClearColor(0, 0, 0, 0);
   glShadeModel(GL_SMOOTH);
   glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
   
+
+   //    glDisable(GL_COLOR_MATERIAL);
+  //gluLookAt (0.0, 0.0, 21.0, 0.0, 0.0, -100.0, 0.0, 1.0, 0.0);
+
+  glWindowPos2i(0, 0);
+
   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
   glEnable(GL_BLEND);
-  
-  //    glDisable(GL_COLOR_MATERIAL);
-  glViewport(0, 0, (GLsizei) w, (GLsizei) h);
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-  gluPerspective(65, (GLfloat) w/(GLfloat) h, 0.01, 20000.0);
-  //gluOrtho2D(0.0, (GLdouble) w, 0.0, (GLdouble) h);
-  glMatrixMode(GL_MODELVIEW);
-  glLoadIdentity();
-  glWindowPos2i(0, 0);
 
   export_symbols();
 }
+
+
--- scm/3d.scm
+++ scm/3d.scm
@@ -13,6 +13,28 @@
 ;; this needs to be fixed in the core guile
 (define <point> <top>) 
 (define <generalized-vector> <top>)
+(define <quaternion> <pair>)
+
+(define (quaternion real imag)
+  (cons real imag))
+
+(define (quaternion-real q)
+  (car q))
+
+(define (quaternion-imag q)
+  (cdr q))
+
+(define-method (re (q <quaternion>))
+  (quaternion-real q))
+
+(define-method (im (q <quaternion>))
+  (quaternion-imag q))
+
+(define-method (re (c <complex>))
+  (real-part c))
+
+(define-method (im (c <complex>))
+  (imag-part c))
 
 (define (array-map proc first . rest)
   (let ((dest (apply make-typed-array (array-type first) *unspecified* 
@@ -26,9 +48,28 @@
 (define-method (- (p <point>) . rest)
   (apply array-map - p rest))
 
-(define-method (- (n <number>) (p <point>)) ; this has to be added to support
-  (array-map (lambda(x)(- n x)) p)) ; unary minus, as guile probably implements
-;; it like that (define (- first . rest) (if (null? rest) (- 0 first) ...))
+; this has to be added to support unary minus, as guile probably 
+; implements it like as (define (- (first <number>)) (- 0 first)) ...
+(define-method (- (n <number>) (p <point>))
+  (array-map (lambda(x)(- n x)) p)) 
+
+(define (quaternion-multiply2 p q)
+  (cons (- (* (re p) (re q))
+	   (* (im p) (im q)))
+	(+ (* (re p) (im q))
+	   (* (re q) (im p))
+	   (* (wedge3x3 (im p) (im q))))))
+
+(define-method (* (p <quaternion>) . rest)
+  (fold (lambda(q p)(quaternion-multiply2 p q)) p rest))
+
+(define-generic ~)
+
+(define-method (~ (c <complex>))
+  (make-rectangular (real-part c) (- (imag-part c))))
+
+(define-method (~ (q <quaternion>))
+  (cons (car q) (- (cdr q))))
 
 (define-symmetric-method (* (s <number>) (p <point>))
   (array-map (lambda(x)(* x s)) p))
@@ -39,32 +80,69 @@
 (define-method (* (u <generalized-vector>) (v <generalized-vector>))
   (dot u v))
 
+(define-method (* (p <point>))
+  p)
+
 (define-method (* (m1 <array>) (m2 <array>) . rest)
   (apply matrix-mul m1 (cons m2 rest)))
 
 (define-class <3d> ()
-  (position #:init-value #(0 0 0))
-  (orientation #:init-value #(1 0 0 0))
-  (shape #:init-value '())
-  (mesh #:init-value '()))
+  (position #:init-value #f32(0 0 0))
+  (orientation #:init-value '(0 . #f32(1 0 0))))
 
-(define-method (draw (object <3d>))
-  (translate-view! #[object 'position])
-  (rotate-view! #[object 'orientation]))
+(define-class <3d-cam> (<3d>)
+  (fovy #:init-value 70.0))
+
+(define-class <3d-shape> (<3d>)
+  (shape #:init-value '()))
+
+(define-class <3d-mesh> (<3d-shape>)
+  (mesh #:init-thunk 
+	(lambda()(with-input-from-file "3d/cube.3d" read))))
+
+
+(define (draw-mesh mesh)
+  (match mesh
+    (('mesh . definition)
+     (for-each (match-lambda
+		(('vertices (? array? array))
+		 (set-vertices-array! array))
+		(('colors (? array? array))
+		 (set-colors-array! array))
+		(('faces . faces)
+		 (for-each (match-lambda 
+			    ((type array)
+			     (draw-faces! type array)))
+			   faces)))
+	       definition))
+    (else
+     (display `(no-match ,else)))))
+		 
+  
+(define-method (draw (object <3d-mesh>))
+  (push-matrix!)
+  ;(translate-view! #[object 'position])
+  ;(rotate-view! #[object 'orientation])
+  (draw-mesh #[object 'mesh])
+  (pop-matrix!))
 
 (define-class <3d-view> (<widget>)
-  (camera #:init-thunk (lambda()(make <3d>)))
+  (camera #:init-thunk (lambda()(make <3d-cam>)))
   (objects #:init-value '()))
 
 (define-method (draw (view <3d-view>))
   (let ((original-viewport (current-viewport)))
     (set-viewport! #[view 'x] #[view 'y] #[view 'w] #[view 'h])
     (push-matrix!)
-    (translate-view! #[#[view 'camera] 'position])
-    (rotate-view! #[#[view 'camera] 'orientation])
+    (perspective-projection! #[view : 'camera : 'fovy])
+    (translate-view! #[view : 'camera : 'position])
+    (rotate-view! #[view : 'camera : 'orientation])
     (for object in #[view 'objects]
 	 (draw object))
     (pop-matrix!)
     (apply set-viewport! original-viewport)))
+
+(define-method (add-object! (view <3d-view>) (object <3d>))
+  (set! #[view 'objects] (cons object #[view 'objects])))
 
 (display "loaded 3d.scm\n")
