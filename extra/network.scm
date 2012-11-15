a6ce97bb266997699866a774b1403101f7a31350
Maciek Godek <godek.maciek@gmail.com>
2012-11-15 18:32:52 +0100

    Adding a 'network' module that defines various routines used by server, which is also being added with this commit. It is yet unattached to the main application in any way, and is only needed to ensure the future organization of files

---
+++ extra/network.scm
@@ -0,0 +1,144 @@
+(define-module (extra network)
+  #:use-module (srfi srfi-1)
+  #:use-module (srfi srfi-2)
+  #:use-module (srfi srfi-11)
+  #:use-module (extra ref)
+  #:use-module (extra common)
+  #:use-module (extra time)
+  #:use-module (ice-9 match)
+  #:use-module (oop goops)
+  #:use-module ((rnrs) :version (6))
+  #:export (
+	    make-client-protocol
+	    handle-packet!
+	    input-available
+	    handle-clients
+	    make-server-cycle
+	    <socket>
+	    <address>
+	    )
+  #:export-syntax (define-protocol-generator 
+		    protocol-add! 
+		    protocol-remove!
+		    proc))
+
+(define <socket> <file-input-output-port>)
+(define <address> <vector>)
+(define <protocol> <hashtable>)
+
+(define (transmit recipent data)
+  (match-let (((socket . address) recipent))
+    (sendto socket data address)))
+
+(define (receive! sender  buffer)
+  (match-let (((socket . address) sender))
+    (recvfrom! socket buffer)))
+
+(define (make-client-protocol typehash)
+  (let ((protocol (make-hash-table))
+	(objects (make-hash-table)))
+    (set! #[protocol 'objects] objects)
+    (set! #[protocol 'set-slots!]
+	  (lambda (id . slots)
+	    (and-let* ((object #[objects id]))
+	      (for (name value) in slots
+		   (slot-set! object name value)))))
+    (set! #[protocol 'add!]
+	  (lambda (type id . slots)
+	    (and-let* ((type #[typehash type])
+		       (object (make type)))
+	      (for (name value) in slots
+		   (slot-set! object name value))
+	      (set! #[objects id] object))))
+    (set! #[protocol 'remove!]
+	  (lambda (id)
+	    (hash-remove! objects id)))
+    protocol))
+
+(define (input-available socket seconds)
+  (match-let (((reads () ()) 
+	       (select `(,socket) '() '() seconds)))
+    (not (null? reads))))
+
+(define (handle-packet! socket clients handle-new-client)
+  (define buffer (make-bytevector 1024))
+  (match-let* (((numread . address) (recvfrom! socket buffer))
+	       (data (substring (utf8->string buffer) 0 numread))
+	       (client-env (hash-ref clients address)))
+    (cond ((and (not client-env) handle-new-client)
+	   (set! client-env (handle-new-client address))
+	   (if client-env
+	       (hash-set! clients address client-env))))
+    (if (and client-env address)
+	(let ((packet (safely (read (open-input-string data)))))
+	  (match packet
+	    ((proc . args)
+	     (let ((result (safely (apply (hash-ref client-env proc) args))))
+	       (if (not (unspecified? result))
+		   (sendto socket (string->utf8 (with-output-to-string 
+						  (\ display result)))
+			   address)))))))))
+
+(define-method (handle-clients (socket <socket>) 
+			       (clients <protocol>) 
+			       (register-protocol <procedure> ) 
+			       (period <ticks>))
+  (pass-ticks-left 
+   period 
+   (lambda (ticks-left)
+     (if (input-available socket  (ticks->seconds ticks-left))
+	 (handle-packet! socket clients register-protocol)))))
+
+(define-syntax proc
+  (syntax-rules ()
+    ((_ args body ...)
+     (let ((p (lambda args body ...)))
+       (set-procedure-property! p 'source '(lambda args body ...))
+       p))))
+
+(define-syntax protocol-add!
+  (syntax-rules ()
+    ((_ protocol-name ((fname arg ...) body ...) ...)
+     (begin 
+       (hash-set! protocol-name (quote fname)
+		  (proc (arg ...) body ...))
+       ...))))
+
+(define-syntax protocol-remove!
+  (syntax-rules ()
+    ((_ protocol-name fname1 ...)
+     (for-each (proc (sym)(hash-remove! protocol-name sym)) 
+	       (list (quote fname1) ...)))))
+
+(define-syntax define-protocol-generator
+  (syntax-rules ()
+    ((_ (protocol-name client-address)
+	bindings
+	(((fname arg ...) body ...) ...))
+     (define protocol-name 
+       (lambda (client-address)
+	 (let ((protocol-name (make-hash-table)))
+	   (let* bindings
+	     (hash-set! protocol-name (quote fname)
+			(proc (arg ...) body ...))
+	     ...)
+	   protocol-name))))))
+
+(define-method 
+  (make-server-cycle (socket <socket>) 
+		     (clients <hashtable>) 
+		     (register-protocol <procedure>)
+		     (update-world <procedure> #;obj->obj)
+		     (respond <procedure> #;cn,pr->?)
+		     (period <seconds>))
+  (let ((ticks (seconds->ticks period)))
+    (lambda ()
+      (handle-clients socket clients register-protocol 
+		      (seconds->ticks period))
+      (update-world)
+      (for-each (match-lambda ((address . protocol)
+			       (respond socket address protocol)))
+		(hash-map->list cons clients)))))
+
+
+  
---
+++ server.scm
@@ -0,0 +1,146 @@
+#!/usr/bin/guile -s
+!#
+(set! %load-path (append (list "." "..")  %load-path))
+
+(use-modules 
+ (oop goops)
+ (srfi srfi-1) (srfi srfi-2) (srfi srfi-11) 
+ (ice-9 match) (ice-9 format) (ice-9 optargs) 
+ (ice-9 pretty-print) (ice-9 local-eval) (ice-9 regex)
+ (ice-9 session)
+ (system base compile) (system syntax)
+ (extra ref) (extra common) (extra network) (extra math)
+ ((rnrs) :version (6)))
+
+(define *world* (make-hash-table))
+(define *users* (make-hash-table))
+
+(define *logged-users* '())
+
+(define-class <goose> ()
+  (owners #:init-value #f #:init-keyword #:owners)
+  (private-slots #:init-value '())
+  (id #:init-thunk gensym))
+
+(define-class <player> (<goose>) 
+  (health #:init-value 100.0)
+  (reload-time-left #:init-value 0)
+  (position #:init-thunk 
+	    (lambda()(random-array #:type 'f32 #:range 5.0 3)))
+  (orientation #:init-value '(1.0 . #f32(0 0 0)))
+  (velocity #:init-thunk 
+	    (lambda()(random-array #:type 'f32 #:range 0.001 3)))
+  (angular-velocity #:init-value #f32(0 0 0)))
+
+;; server is stupid and reliable; client is fancy and fallable
+
+(define-method (jump! (p <player>))
+  (increase! #[p 'position] #(0 1 0)))
+
+(define-method (shoot! (p <player>))
+  (set! #[p 'reload-time-left] 20))
+
+(define-method (turn! (p <player>))
+  (increase! #[p 'position] #(0 1 0)))
+
+(define-method (crouch! (p <player>))
+  (set! #[p 'reload-time-left] 20))
+
+(define-method (walk! (p <player>))
+  (set! #[p 'reload-time-left] 20))
+
+(define-method (update! (p <player>))
+  (increase! #[p 'position] #[p 'velocity]))
+
+(define-method (spawn-object! world player)
+  (hash-set! world #[player 'id] player))
+
+(define (procedure-args proc)
+  (let
+
+  (match (procedure-source proc)
+    (('lambda args body ...)
+     args)
+    (else
+     (match (procedure-minimum-arity proc)
+       ((required optional rest)
+	(append (make-list required '_)
+		(if (> optional 0) (cons #:optional (make-list optional '_)) '())
+		(if rest '_ '())))
+       (else
+	'(..?))))))
+
+(for-each
+ (match-lambda((username password)
+	       (hash-set! *users* username password)))
+ '((panicz k0byl4)
+   (drcz 12345)
+   (polak żółć)))
+
+(define (original-symbol symbol)
+  (if (symbol? symbol)
+      (or (and-let* ((match (string-match "-[0-9]+$" (symbol->string symbol))))
+	    (string->symbol (regexp-substitute #f match 'pre)))
+	symbol)
+      symbol))
+
+(define-protocol-generator (kutasa-protocol connection)
+  ((username #f)
+   (player #f)
+   (address connection))
+  (((login name password)
+    (if (and (not username) password 
+	     (equal? (hash-ref *users* name) password))
+	(begin
+	  (set! username name)
+	  (set! *logged-users* (cons name *logged-users*))
+	  #t)
+	#f))
+   ((join)
+    (if username
+	(begin
+	  (set! player (make <player> #:owners `(,address)))
+	  (spawn-object! *world* player)
+	  (protocol-add! kutasa-protocol
+			 ((jump) (jump! player))
+			 ((shoot) (shoot! player))
+			 ((turn degs) (turn! player degs))
+			 ((crouch) (crouch! player))
+			 ((walk) (walk! player)))
+	  #t)
+	'not-logged-in))
+   ((leave)
+    (set! player #f)
+    (protocol-remove! kutasa-protocol jump shoot turn crouch walk))
+   ((prn)
+    '(prn))
+   ((display message)
+    (display message))
+   ((describe-protocol)
+    (hash-map->list (lambda (name proc)
+		      (cons name
+			    (map original-symbol 
+				 (procedure-args proc))))
+		    kutasa-protocol))
+   ((logout)
+    (if username (set! username #f))
+    (set! *logged-users*
+	  (remove (\ equal? _ username) *logged-users*)))))
+
+(let ((socket (socket PF_INET SOCK_DGRAM 0)))
+  (bind socket AF_INET INADDR_ANY 41337)
+  (let ((server-cycle (make-server-cycle 
+		       socket
+		       (make-hash-table)
+		       kutasa-protocol
+		       (lambda()
+			 (display "update!\n")
+			 (for-each update! 
+				   (hash-map->list
+				    (lambda(id object)
+				      object)
+				    *world*)))
+		       (lambda x x)
+		       3
+		       )))
+  (while #t (server-cycle))))
