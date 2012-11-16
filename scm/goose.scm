1379671b927a293653d03d571514c3fcfe5e3f28
Maciek Godek <godek.maciek@gmail.com>
2012-11-16 12:30:55 +0100

    The interface of goose-view has been adjusted to the one used by the network. It hasn't been tested anyway.

--- scm/goose.scm
+++ scm/goose.scm
@@ -1,10 +1,15 @@
 (display "loading goose.scm\n")
 
+(use-modules (extra network))
+
 (define-class <goose-client> ()
-  (connection #:init-value #f)
+  (socket.address #:init-value #f)
   #;(mutex #:init-thunk make-mutex)
   (protocol #:init-thunk make-hash-table)
   (type-hash #:init-thunk make-hash-table)
+  ;; type-hash contains a hash whose keys are type-names (symbols)
+  ;; and values are GOOPS types, thus making it closer
+
   ;; the keywords the class is meant to be
   ;; initialized with:
   ;;   #:address "nu.mb.e.r:port"
@@ -12,6 +17,19 @@
   ;;   #:username "name"
   ;;   #:password "phrase"
   )
+
+(define (resolve-address string)
+  (let-values (((address port) (match (string-split string #\:)
+				 ((address ... port)
+				  (values (string-join address "") 
+					  (string->number port)))
+				 ((address)
+				  (values address 41337)))))
+    (let ((address (if (string-match "^([0-9]{1,3}[.]){3}[0-9]{1,3}"
+				     address)
+		       (inet-aton address)
+		       INADDR_LOOPBACK)))
+      (cons address port))))
 
 (define-method (initialize (this <goose-client>) args)
   (next-method)
@@ -21,40 +39,48 @@
        (username "anonymous") ;; currently these are dismissed
        (password "")
        (types '()))
-    (for-each (match-lambda ((name type)
-			     (hash-set! #[this 'type-hash] name type)))
-	      types)
-    (let ((connection (make-connection address))
-	  (protocol (make-client-protocol #[this 'type-hash]))
+    (for (name type) in types 
+	 (hash-set! #[this 'type-hash] name type))
+    (match-let* (((address . port) (resolve-address address))
+		 (socket (socket PF_INET SOCK_DGRAM 0))
+		 (address (make-socket-address AF_INET address port))
+		 (protocol (make-client-protocol #[this 'type-hash]))
 	  #;(mutex #[this 'mutex])
 	  (buffer (make-bytevector 1024)))
-      (set! #[this 'connection] connection)
+      (set! #[this 'socket.address] (cons socket address))
       (set! #[this 'protocol] protocol)
-      (let ((code (register-userevent ; this code is executed by the main thread
-		   (lambda (data connection)
+      (let ((code (register-userevent ; this code is executed 
+		   (lambda (data socket.address) ; by the main thread
 		     #;(lock-mutex mutex)
-		     (match-let (((proc . args) (with-input-from-string data read)))
+		     (match-let (((socket . address) socket.address)
+				 ((proc . args) 
+				  (with-input-from-string data read)))
 		       (let ((result (apply #[protocol proc] args)))
 			 (if (not (unspecified? result))
-			     (transmit connection 
-				       (string->utf8 (with-output-to-string (lambda()(write result))))))))
+			     (sendto socket
+				     (with-output-to-utf8 
+				      (\ write result))
+				     address))))
 		     #;(unlock-mutex mutex)))))
 	(call-with-new-thread 
 	 (lambda ()
 	   (while #t
-	     (match-let (((nread . address) (receive! connection buffer)))
+	     (match-let (((nread . address)(recvfrom! socket buffer)))
 	       (generate-userevent 
 		code 
 		(substring (utf8->string buffer) 0 nread) 
-		connection)))))
+		(cons socket address))))))
 	this))))
 
 (define-class <goose-view> (<3d-view> <goose-client>))
 
 (define-method (draw (this <goose-view>))
   #;(lock-mutex #[this 'mutex])
-  (set! #[this 'objects] (hash-map->list (lambda (key value) value)  #[this : 'protocol : 'objects]))
+  (set! #[this 'objects] (hash-map->list 
+			  (lambda (key value) value) 
+			  #[this : 'protocol : 'objects]))
   (next-method)
   #;(unlock-mutex #[this 'mutex]))
+
 
 (display "loaded goose.scm\n")
