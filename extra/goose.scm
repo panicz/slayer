1b8c3ee1f055479fa7613c1dc2dc1e501ae8d129
Maciek Godek <godek.maciek@gmail.com>
2012-11-19 16:04:58 +0100

    A new read-hash extension has been added, for #\<, that makes the reader interpret expressions like #<φ> as (φ), where the count of '<' inside φ must equate the count of '>', unless the eof object occurs first.
    Simultaneously, the id of instances of <goose> and its descendants
    is no longer generated using gensym. Instead, it's its address,
    as returned by the display function (reinterpreted as a number).
    A minor inconvenience might be that the display procedure for GOOPS
    objects displays a hex address, and by default the display procedure
    uses decimal representation, so these two seem different to the human
    eye. Fortunately, that's usually meaningless.

--- extra/goose.scm
+++ extra/goose.scm
@@ -5,6 +5,7 @@
   :use-module (oop goops)
   :use-module (ice-9 match)
   :use-module (ice-9 optargs)
+  :use-module (ice-9 regex)
   ;:use-module (extra slayer)
   :use-module (extra ref)
   :use-module (extra common)
@@ -16,19 +17,64 @@
 	   state-of
 	   ))
 
+(read-hash-extend 
+ #\<
+ (lambda (char port)
+   (let* ((chars->data (lambda(l)
+			 (let ((s (list->string (reverse l))))
+			   (cond 
+			    ((string-match "^[0-9a-fA-F]+$" s)
+			     (string->number (string-append "#x" s)))
+			    (else
+			     (string->symbol s))))))
+	  (return (lambda(tokens current-token)
+		    (reverse 
+		     (if (null? current-token) 
+			 tokens
+			 (cons (chars->data current-token) tokens))))))
+     (let loop ((level 0)
+		(current-token '())
+		(tokens '()))
+       (let ((char (read-char port)))
+	 (cond ((eof-object? char)
+		(return tokens current-token))
+	       ((char-whitespace? char)
+		(if (null? current-token)
+		    (loop level current-token tokens)
+		    (loop level '() (cons (chars->data current-token)
+					  tokens))))
+	       ((equal? char #\<)
+		(loop (1+ level) (cons char current-token) tokens))
+	       ((equal? char #\>)
+		(if (= level 0)
+		    (return tokens current-token)
+		    (loop (1- level) (cons char current-token)
+			  tokens)))
+	       (else
+		(loop level (cons char current-token) tokens))))))))
+
 (define-class <goose> ()
   (owners #:init-value #f #:init-keyword #:owners)
   (context #:init-value #f) ; the subspace to which it belongs
   (private-slots #:init-value '()) ; slots visible only to the owners
   (client-slots #:init-value '())
-  (id #:init-thunk (\ gensym "g-")))
+  (id #:init-value 0))
 
-(define (state-of object)
+(define-method (initialize (this <goose>) args)
+  (next-method)
+  (match-let (((type ... id)
+	       (with-input-from-string (with-output-to-string (\ display this)) read)))
+    (set! #[this 'id] id)))
+
+(define* (state-of object #:optional (owner #t))
   (map (lambda (slot) (list slot #[object slot]))
        (lset-difference equal?
 			(map first (class-slots (class-of object)))
 			(map first (class-slots <goose>))
-			#[object 'client-slots])))
+			#[object 'client-slots]
+			(if owner
+			    #[object 'private-slots]
+			    '()))))
 
 (define-method (objects-visible-to object)
   (list object))
