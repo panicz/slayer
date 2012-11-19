30741bfda3aae79f6a6e9ce6ddd96a24f84a6693
Maciek Godek <godek.maciek@gmail.com>
2012-11-19 16:06:32 +0100

    Adjusted the code to the 70-character terminal displays

--- extra/ref.scm
+++ extra/ref.scm
@@ -78,40 +78,46 @@
 (define-method (ref (object <top>) (key <symbol>))
   (sref object key))
 
-(read-hash-extend #\[
-		  (letrec ((process-:-sequence (lambda(sequence initial)
-						 (match sequence
-						   (()
-						    initial)
-						   ((': key . rest)
-						    (process-:-sequence rest `(ref ,initial ,key)))))))
-		    (let ((process (match-lambda
-				    (('_ key)
-				     `(lambda(_)(ref _ ,key)))
-				    ((object key)
-				     `(ref ,object ,key))
-				    (('_ ': key . rest)
-				     `(lambda(_),(process-:-sequence rest `(ref _ ,key))))
-				    ((object ': key . rest)
-				     (process-:-sequence rest `(ref ,object ,key)))
-				    ((seq start '.. end)
-				     `(substring ,seq ,start ,end))
-				    ((seq start ': count)
-				     `(substring ,seq ,start ,(min (string-length seq) (+ start count))))
-				    ((array . indices)
-				     `(aref ,array (list ,@indices)))
-				    (default
-				      default))))
-		      (lambda (char port) 
-			(let loop ((exp '()))
-			  (let ((char (read-char port)))
-			    (cond ((char-whitespace? char)
-				   (loop exp))
-				  ((eq? char #\])
-				   (process (reverse exp)))
-				  (#t
-				   (unread-char char port)
-				   (loop (cons (read port) exp))))))))))
+(read-hash-extend 
+ #\[
+ (letrec ((process-:-sequence (lambda(sequence initial)
+				(match sequence
+				  (()
+				   initial)
+				  ((': key . rest)
+				   (process-:-sequence 
+				    rest 
+				    `(ref ,initial ,key)))))))
+   (let ((process (match-lambda
+		      (('_ key)
+		       `(lambda(_)(ref _ ,key)))
+		    ((object key)
+		     `(ref ,object ,key))
+		    (('_ ': key . rest)
+		     `(lambda(_)
+			,(process-:-sequence rest `(ref _ ,key))))
+		    ((object ': key . rest)
+		     (process-:-sequence rest `(ref ,object ,key)))
+		    ((seq start '.. end)
+		     `(substring ,seq ,start ,end))
+		    ((seq start ': count)
+		     `(substring ,seq ,start 
+				 ,(min (string-length seq) 
+				       (+ start count))))
+		    ((array . indices)
+		     `(aref ,array (list ,@indices)))
+		    (default
+		      default))))
+     (lambda (char port) 
+       (let loop ((exp '()))
+	 (let ((char (read-char port)))
+	   (cond ((char-whitespace? char)
+		  (loop exp))
+		 ((eq? char #\])
+		  (process (reverse exp)))
+		 (#t
+		  (unread-char char port)
+		  (loop (cons (read port) exp))))))))))
 
 (if (string<? (version) "2.0.0")
     (read-hash-extend #\;
