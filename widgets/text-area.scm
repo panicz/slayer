09d4865203caa5cf3ae1915f149f1905ccaf4b98
Maciek Godek <godek.maciek@gmail.com>
2013-04-13 22:02:03 +0200

    widgets/widgets.scm has been split into three separate files: base.scm (defintion of <widget>, <extended-widget> and elementary functions), bitmap.scm (definition of <bitmap> and related functions) and text-area.scm (<text-area> et al.)

--- widgets/text-area.scm
+++ widgets/text-area.scm
@@ -1,170 +1,18 @@
-(define-module (widgets widgets)
+(define-module (widgets text-area)
   #:use-module (oop goops)
   #:use-module (extra ref)
   #:use-module (extra common)
-  #:use-module (slayer font)
+  #:use-module (slayer)
   #:use-module (slayer video)
   #:use-module (slayer input)
   #:use-module (slayer image)
-  #:use-module (slayer)
-  
-  #:export (
-	    update! 
-	    draw 
-	    area 
-	    absolute-area
-	    in-area?
-	    add-child! 
-	    remove-child!
-	    ancestors
-	    widget-nested-find
-	    
-	    make-button
-	    make-container
-	    make-image
-
-	    <widget>
-	    <extended-widget>
-	    <bitmap>
-	    <text-area>
-	    move-cursor!
-	    delete-char!
+  #:use-module (slayer font)
+  #:use-module (widgets base)
+  #:export (<text-area>
 	    make-text-area
-	    input-text!
-	    ))
-
-(define-generic update!)
-(define-generic draw)
-(define-generic area)
-(define-generic add-child!)
-(define-generic remove-child!)
-
-(define-class <widget> ()
-  (parent #:init-value #f #:init-keyword #:parent)
-  (children #:init-value '() #:init-keyword #:children)
-  (click #:init-value noop #:init-keyword #:click)
-  (unclick #:init-value noop #:init-keyword #:unclick)
-  (right-click #:init-value noop #:init-keyword #:right-click)
-  (right-unclick #:init-value noop #:init-keyword #:right-unclick)
-  (mouse-over #:init-value noop #:init-keyword #:mouse-over)
-  (mouse-out #:init-value noop #:init-keyword #:mouse-out)
-  (drag #:init-value noop #:init-keyword #:drag)
-  (update!  #:init-value noop #:init-keyword #:update)
-  (activate #:init-value noop #:init-keyword #:activate)
-  (deactivate #:init-value noop #:init-keyword #:deactivate)
-  (x #:init-value 0 #:init-keyword #:x)
-  (y #:init-value 0 #:init-keyword #:y)
-  (w #:init-value 0 #:init-keyword #:w)
-  (h #:init-value 0 #:init-keyword #:h))
-
-(define-class <extended-widget> (<widget>)
-  (data #:init-thunk make-hash-table))
-
-(define-method (ancestors (w <widget>))
-  (or (and-let* ((parent #[w 'parent]))
-	(cons parent (ancestors parent)))
-      '()))
-
-(define-method (area (w <widget>))
-  (list #[ w 'x ] #[ w 'y ] #[ w 'w ] #[ w 'h ]))
-
-(define-method (absolute-area (w <widget>))
-  (let ((widgets (cons w (ancestors w))))
-    (list (apply + (map #[_ 'x] widgets))
-	  (apply + (map #[_ 'y] widgets))
-	  #[w 'w] #[w 'h])))
-
-(define-method (draw (w <widget>))
-  (for-each draw (reverse #[ w 'children ])))
-
-(define-method (add-child! (parent <widget>) (child <widget>))
-  (set! #[ parent 'children ] (cons child #[ parent 'children ]))
-  (set! #[ parent 'w ] (max #[ parent 'w ] 
-			    (+ #[ child 'x ] #[ child 'w ])))
-  (set! #[ parent 'h ] (max #[ parent 'h ] 
-			    (+ #[ child 'y ] #[ child 'h ])))
-  (set! #[ child 'parent ] parent))
-
-(define-method (remove-child! (parent <widget>) (child <widget>))
-  (set! #[ child 'parent ] #f)
-  (set! #[ parent 'children ] (delete child #[ parent 'children ])))
-
-(define (in-area? point area)
-  (match-let
-   (((x y w h) area)
-    ((px py) point))
-   (and (<= x px (+ x w)) (<= y py (+ y h)))))
-
-(define (widget-nested-find condition widget)
-  (if (not (condition widget))
-      #f
-      (let ((w (find condition (slot-ref widget 'children))))
-	(if (not w)
-	    widget
-	    (let ((c (widget-nested-find condition w)))
-	      (if (not c) w c))))))
-
-(define-class <bitmap> (<widget>)
-  (image #:init-keyword #:image))
-
-(define-method (draw (i <bitmap>))
-  (draw-image #[ i 'image ]
-	      (+ (or #[#[ i 'parent ] 'x] 0) #[ i 'x ]) 
-	      (+ (or #[#[ i 'parent ] 'y] 0) #[ i 'y ])))
-
-(define* (make-button #:key (text "button") (x 0) (y 0) (w #f) (h #f))
-  (let ((normal (render-text text *default-font* #xffffff #xff0000))
-	(over (render-text text *default-font* #xffffff #x00ff00))
-	(clicked (render-text text *default-font* #xffffff #x0000ff)))
-    (let ((button (make <bitmap> #:image normal #:x x #:y y 
-			#:w (or w (image-width normal))
-			#:h (or h (image-height normal)))))
-      (set! #[ button 'mouse-over ] 
-	    (lambda e (set! #[ button 'image ] over )))
-      (set! #[ button 'mouse-out ] 
-	    (lambda e (set! #[ button 'image ] normal )))
-      (set! #[ button 'click ] 
-	    (lambda e (set! #[ button 'image ] clicked )))
-      (set! #[ button 'unclick ] #[ button 'mouse-over ])
-      (set! #[ button 'drag ] noop)
-      button)))
-
-(define* (make-container #:key x y (name "menu") (content '()))
-  (let ((container (make <widget>))
-	(label (make-button #:text name)))
-    (set! #[label 'drag] (lambda (x y xrel yrel)		 
-			   (increase! #[ container 'x ] xrel)
-			   (increase! #[ container 'y ] yrel)))
-    (for child in (append content `(,label))
-	 (add-child! container child))
-    ;(add-child! container (make-button #:x 0 #:y 0 #:text name))
-    (let ((top 0))
-      (for w in #[container 'children]
-	   (set! #[container 'w] (max #[w 'w] #[container 'w]))
-	   (set! #[w 'y] top)
-	   (set! top (+ top #[w 'h])))
-      (set! #[container 'h] top))
-    container))
-
-(define (make-image image x y)
-  (let ((image (make <bitmap> #:image image #:x x #:y y 
-		     #:w (image-width image) 
-		     #:h (image-height image))))
-    (set! #[ image 'drag ]
-	  (lambda (x y xrel yrel)		 
-	    (increase! #[ image 'x ] xrel)
-	    (increase! #[ image 'y ] yrel)))
-    #;(set! #[ image 'mouse-over ]
-	  (lambda (x y xrel yrel)
-	    (format #t "now mouse is over ~s\n" image)))
-    #;(set! #[ image 'mouse-out ]
-	  (lambda (x y xrel yrel)
-	    (format #t "mouse is no longer over ~s\n" image)))
-    image))
+	    *default-font*))
 
 (define *default-font* (load-font "./art/VeraMono.ttf" 12))
-
-;(set-font-style! *default-font* 1)
 
 (define-class <text-area> (<widget>)
   (lines #:init-value '#(""))
@@ -189,9 +37,9 @@
 	image)))
 
 (define-method (draw (t <text-area>))
-  (let* ((font #[ t 'font ])
+  (let* ((font #[t 'font])
 	 (line-skip (font-line-skip font))
-	 (lines #[ t 'lines ])
+	 (lines #[t 'lines])
 	 (space (render-text "_" font))
 	 (cursor (rectangle 2 line-skip #x20eeaa22)))
       (for-each (lambda(line top)
@@ -200,12 +48,12 @@
 			  (max #[ t 'w ] (image-width image)))
 		    (draw-image image 
 				(+ (or #[ #[ t 'parent ] 'x ] 0) 
-				   #[ t 'x ]) 
-				(+ (or #[ #[ t 'parent ] 'y ] 0) 
-				   #[ t 'y ] top))))
+				   #[t 'x]) 
+				(+ (or #[#[t 'parent] 'y] 0) 
+				   #[t 'y] top))))
 		(vector->list lines)
 		(iota (vector-length lines) 0 line-skip))
-      (if (equal? (current-output-port) #[ t 'port ])
+      (if (equal? (current-output-port) #[t 'port])
 	  (draw-image cursor 
 		      (+ #[t 'x] (* (image-width space) 
 				    (port-column #[ t 'port ])) )
