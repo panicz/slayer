8f9397675ad9a6018331cde0a1694730f4dad481
Maciek Godek <godek.maciek@gmail.com>
2013-04-28 16:37:52 +0200

    Added a C++-compatibile version of WARN_ONCE, as well as min* and max* functions for i-ntegers and f-loats

--- utils.h
+++ utils.h
@@ -18,14 +18,43 @@
 #define FATAL(msg, ...) do { fprintf(stderr, "FATAL ERROR: " msg "\n", ## __VA_ARGS__ ); exit(0); } while(0)
 #define NELEMS(a) ((int)(sizeof(a)/sizeof(a[0])))
 #define WARN(msg, ...) fprintf(stderr, "[ %09d ] %s/%s: " msg "\n", now(), __FILE__, __FUNCTION__, ## __VA_ARGS__ )
-#define WARN_UPTO(n, msg, ...) \
+
+#ifdef __cplusplus
+#define WARN_UPTO(n, msg, ...)						\
+  ({static int __warn_counter_##n;					\
+  if(__warn_counter_##n++ < n) {					\
+    OUT("[ %09d ] %s/%s: " msg " (warning %i of %i)",			\
+	now(), __FILE__, __FUNCTION__, ## __VA_ARGS__,			\
+	__warn_counter_##n, n);					\
+  }})
+#else
+#define WARN_UPTO(n, msg, ...)	  \
   ({ void __fn__(const char *f) { \
       static int c=0; \
       if(c++ < n) OUT("[ %09d ] %s/%s: " msg " (warning %i of %i)", now(), __FILE__, f, ## __VA_ARGS__, c, n); \
     } __fn__(__FUNCTION__); })
+#endif
+
+
 #define WARN_ONCE(msg, ...) WARN_UPTO(1, msg, ## __VA_ARGS__ )
 #define TOSTRING(x) STR(x)
 #define TRY(f) if((f) == -1) { perror(__FILE__ ", " TOSTRING(__LINE__)  " [" #f "]"); }
+
+
+#define DEF_MINMAX(type, suffix)		\
+  static inline					\
+  type max##suffix(type a, type b) {		\
+    return (a >= b) ? a : b;			\
+  }						\
+  static inline					\
+  type min##suffix(type a, type b) {		\
+    return (a < b) ? a : b;			\
+  }
+
+DEF_MINMAX(int, i);
+DEF_MINMAX(float, f);
+
+#undef DEF_MINMAX
 
 
 //do { } while(0)
