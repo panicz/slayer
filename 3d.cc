c7a9084e26b56b29b8c144e043825b3e5cb7b5fe
Maciek Godek <godek.maciek@gmail.com>
2013-04-28 16:38:55 +0200

    Implemented set-color! procedure for vector/integer arguments

--- 3d.cc
+++ 3d.cc
@@ -1,7 +1,9 @@
+#include "slayer.h"
 #include "extend.h"
 #include "utils.h"
 #include "vmx.hh"
 #include "video.h"
+#include <limits.h>
 
 extern "C" void glWindowPos2i (GLint x, GLint y);
 
@@ -127,6 +129,7 @@ perspective_projection_x(SCM FOVY, SCM ASPECT, SCM NEAR, SCM FAR) {
 
 static SCM
 ortographic_projection_x() {
+  // TODO
   WARN("unimplemented");
   return SCM_UNSPECIFIED;
 }
@@ -240,6 +243,103 @@ init_GLnames() {
 #undef SET_VALUE
 }
 
+static void dont_set_color(const void *unused) { 
+  WARN("Unsupported color setter called");
+}
+static void (*color_setters[SCM_ARRAY_ELEMENT_TYPE_LAST+1][2])(const void *);
+static inline void
+init_color_setters() {
+  int i, dim;
+  for(dim = 0; dim <= 1; dim++) {
+    for(i = 0; i < NELEMS(color_setters); ++i) {
+      color_setters[i][dim] = dont_set_color;
+    }
+  }
+#define SET_COLOR_SETTER(type, dim, gls)		\
+  color_setters[SCM_ARRAY_ELEMENT_TYPE_##type][dim-3]	\
+    = (void (*)(const void *)) glColor##dim##gls##v
+  
+#define SET_COLOR_SETTERS(type, gls)		\
+  SET_COLOR_SETTER(type, 3, gls);		\
+  SET_COLOR_SETTER(type, 4, gls)
+
+  SET_COLOR_SETTERS(S8, b);
+  SET_COLOR_SETTERS(S16, s);
+  SET_COLOR_SETTERS(S32, i);
+  SET_COLOR_SETTERS(F32, f);
+  SET_COLOR_SETTERS(F64, d);
+
+  SET_COLOR_SETTERS(U8, ub);
+  SET_COLOR_SETTERS(U16, us);
+  SET_COLOR_SETTERS(U32, ui);
+
+#undef SET_COLOR_SETTER
+}
+
+static SCM
+set_color_x(SCM value) {
+  if(scm_is_unsigned_integer(value, 0, UINT_MAX)) {
+    SDL_Color c = sdl_color(scm_to_uint(value));
+    glColor4ub(c.r, c.g, c.b, c.unused);
+  }
+  else if(scm_is_uniform_vector(value)) {
+    size_t size = mini(scm_c_uniform_vector_length(value), 4);
+    if(size < 2) {
+      WARN("Invalid uniform vector size: %d", (int) size);
+      return SCM_UNSPECIFIED;
+    }
+    scm_t_array_handle h;
+    scm_array_get_handle(value, &h);
+    (color_setters[h.element_type][size-3])
+      (scm_array_handle_uniform_elements(&h));
+    scm_array_handle_release(&h);
+  }
+  else if(scm_is_vector(value)) {
+
+#define V(n) scm_c_vector_ref(value, n)
+#define UINT(x) scm_to_uint(x)
+#define DBL(x) scm_to_double(x)
+
+    int i, exact = 1;
+    size_t size = mini(scm_c_vector_length(value), 4);
+    if(size < 2) {
+      WARN("Invalid vector size: %d", (int) size);
+      return SCM_UNSPECIFIED;
+    }
+    for(i = 0; i < (int) size; ++i) {
+      if(scm_is_inexact(V(i))) {
+	exact = 0;
+	break;
+      }
+    }
+    switch(size) {
+    case 3:
+      if(exact) {
+	glColor3ui(UINT(V(0)), UINT(V(1)), UINT(V(2)));
+      }
+      else {
+	glColor3d(DBL(V(0)), DBL(V(1)), DBL(V(2)));
+      }
+      break;
+    case 4:
+      if(exact) {
+	glColor4ui(UINT(V(0)), UINT(V(1)), UINT(V(2)), UINT(V(3)));
+      }
+      else {
+	glColor4d(DBL(V(0)), DBL(V(1)), DBL(V(2)), DBL(V(3)));
+      }
+      break;
+    }
+#undef DBL
+#undef UINT
+#undef V
+  }
+  else {
+    WARN("Unsupported argument type");
+  }
+  return SCM_UNSPECIFIED;
+}
+
 static SCM
 draw_faces_x(SCM type, SCM array) {
 #define GET_VALUE(name) \
@@ -294,6 +394,7 @@ export_symbols(void *unused) {
 		      set_colors_array_x);
   EXPORT_PROCEDURE("set-normal-array!", 1, 0, 0, 
 		   set_normal_array_x);
+  EXPORT_PROCEDURE("set-color!", 1, 0, 0, set_color_x);
 
   EXPORT_PROCEDURE("set-texture-coords-array!", 1, 0, 0, 
 		      set_texture_coords_array_x);
@@ -306,6 +407,7 @@ extern "C" void
 init_3d(Uint16 w, Uint16 h) {
   init_GLtypes();
   init_GLnames();
+  init_color_setters();
 
   SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
   glEnable(GL_DEPTH_TEST);
