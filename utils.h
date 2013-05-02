ec0396c2d65bb0e6f27836b9e5b627d13cde471b
Maciek Godek <godek.maciek@gmail.com>
2013-05-02 10:17:34 +0200

    Re-enabled time information in warnings, this time using <clock.h> (which means lower resolution)

--- utils.h
+++ utils.h
@@ -5,25 +5,28 @@
 #include <stdlib.h>
 #include <time.h>
 
+static inline
+unsigned int now() {
+  return (unsigned int) (100 * clock() / CLOCKS_PER_SEC);
+}
 
-#define now() 0
 #define OUT(msg, ...) fprintf(stderr, msg "\n", ## __VA_ARGS__ )
 #define OUT_(msg, ...) fprintf(stderr, msg, ## __VA_ARGS__ )
 #define STR(x) # x
-#define LOG(expr) ({OUT("[ %09d ] %s/%s[%d]: "STR(expr),now(),__FILE__,__FUNCTION__,__LINE__);expr;})
+#define LOG(expr) ({OUT("[ %09u ] %s/%s[%d]: "STR(expr),now(),__FILE__,__FUNCTION__,__LINE__);expr;})
 #define LOGTIME(expr) \
   ({int time=now();expr;time=now()-time;\
-    OUT("[ %09d ] %s/%s,%d: "STR(expr)" (%d ticks)",now(),__FILE__,__FUNCTION__,__LINE__,time);})
+    OUT("[ %09u ] %s/%s,%d: "STR(expr)" (%d ticks)",now(),__FILE__,__FUNCTION__,__LINE__,time);})
 
 #define FATAL(msg, ...) do { fprintf(stderr, "FATAL ERROR: " msg "\n", ## __VA_ARGS__ ); exit(0); } while(0)
 #define NELEMS(a) ((int)(sizeof(a)/sizeof(a[0])))
-#define WARN(msg, ...) fprintf(stderr, "[ %09d ] %s/%s: " msg "\n", now(), __FILE__, __FUNCTION__, ## __VA_ARGS__ )
+#define WARN(msg, ...) fprintf(stderr, "[ %09u ] %s/%s: " msg "\n", now(), __FILE__, __FUNCTION__, ## __VA_ARGS__ )
 
 #ifdef __cplusplus
 #define WARN_UPTO(n, msg, ...)						\
   ({static int __warn_counter_##n;					\
   if(__warn_counter_##n++ < n) {					\
-    OUT("[ %09d ] %s/%s: " msg " (warning %i of %i)",			\
+    OUT("[ %09u ] %s/%s: " msg " (warning %i of %i)",			\
 	now(), __FILE__, __FUNCTION__, ## __VA_ARGS__,			\
 	__warn_counter_##n, n);					\
   }})
@@ -31,7 +34,7 @@
 #define WARN_UPTO(n, msg, ...)	  \
   ({ void __fn__(const char *f) { \
       static int c=0; \
-      if(c++ < n) OUT("[ %09d ] %s/%s: " msg " (warning %i of %i)", now(), __FILE__, f, ## __VA_ARGS__, c, n); \
+      if(c++ < n) OUT("[ %09u ] %s/%s: " msg " (warning %i of %i)", now(), __FILE__, f, ## __VA_ARGS__, c, n); \
     } __fn__(__FUNCTION__); })
 #endif
 
