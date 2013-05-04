c5bb0a0c57357b367c3a16a1eca9629a508b7219
Maciek Godek <godek.maciek@gmail.com>
2013-05-04 18:41:30 +0200

    Another empty header

--- input.h
+++ input.h
@@ -1,13 +1,7 @@
 #ifndef INPUT_H
 #define INPUT_H
-#include "extend.h"
 
-/** initialize and finalize input subsystem */
-extern void input_init();
-extern void input_finish();
 
-/** use the input subsystem to handle events */
-extern SCM input_handle_events();
 
 
 #endif /* INPUT_H */
