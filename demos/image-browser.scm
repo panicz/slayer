9b113540d386a8b83f3ac367b6d51249bd912570
Maciek Godek <godek.maciek@gmail.com>
2013-06-25 22:43:27 +0200

    Small simplification in the image-browser demo

--- demos/image-browser.scm
+++ demos/image-browser.scm
@@ -1,27 +1,25 @@
 (use-modules (slayer) (slayer image) 
-	     (ice-9 popen) (ice-9 rdelim) (ice-9 pretty-print)
 	     (srfi srfi-1) (srfi srfi-2))
 (keydn 'esc quit)
 
-(define (list-directory directory)
-  (let ((dir (opendir directory)))
-    (unfold eof-object? (lambda(f)(string-append directory f)) 
+(define (list-directory path)
+  (let ((dir (opendir path)))
+    (unfold eof-object? (lambda(f)(string-append path f)) 
 	    (lambda x (readdir dir)) (readdir dir))))
 
 (define (file? f) (eq? 'regular (stat:type (stat f))))
 
 (define *image-names* (filter file? (list-directory "/usr/share/pixmaps/")))
-(define *number-of-images* (length *image-names*))
 (define *current-image* #f)
 (define *image-index* 0)
 
-(if (<= *number-of-images* 0)
-    (die "no images found"))
+(if (<= (length *image-names*) 0)
+    (begin (display "no images found\n") (quit)))
 
 (set-display-procedure! (lambda () (draw-image! *current-image* 0 0)))
 
 (define (show-image! i)
-  (set! *image-index* (modulo i *number-of-images*))
+  (set! *image-index* (modulo i (length *image-names*)))
   (and-let* ((image-name (list-ref *image-names* *image-index*))
 	     (image (load-image image-name)))
     (set! *current-image* image)
