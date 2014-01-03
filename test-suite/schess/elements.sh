24271a45263b585d02dfa396c27babc752729502
Maciek Godek <godek.maciek@gmail.com>
2014-01-03 02:51:42 +0100

    Setting GUILE_AUTO_COMPILE to "fresh" for both tests

--- test-suite/schess/elements.sh
+++ test-suite/schess/elements.sh
@@ -2,5 +2,6 @@
 HERE=$(dirname $0)
 ROOT=$HERE/../..
 GUILE_LOAD_PATH="$ROOT/demos:$ROOT/guile-modules" 
+GUILE_AUTO_COMPILE="fresh"
 LC_ALL="C.UTF-8"
 $ROOT/tools/test-module.scm schess elements
--- test-suite/tools/ply23d.sh
+++ test-suite/tools/ply23d.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 HERE=$(dirname $0)
 ROOT=$HERE/../..
+GUILE_AUTO_COMPILE="fresh"
 GUILE_LOAD_PATH="$ROOT/guile-modules" 
 LC_ALL="C.UTF-8"
+
 $ROOT/tools/ply23d.scm $HERE/data/cube.ply
 if [ ! -f $HERE/data/cube.3d ]
     then echo "Failed to generate $HERE/data/cube.3d"
