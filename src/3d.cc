a8c86b980bc8e7654f37ebe10826f794383420df
Maciek Godek <godek.maciek@gmail.com>
2014-02-07 16:35:23 +0100

    renamed "unproject" to "screen->world/coordinates", and added a reciprocal function "world->screen/coordinates"

--- src/3d.cc
+++ src/3d.cc
@@ -301,12 +301,51 @@ translation_matrix(SCM vector) {
   return scm_from_m4x4d(m4x4d(m3x3d(), v4d(v, 1)));
 }
 
+#define VIEWPORT_LIST_TO_STRUCT(list_name, struct_name)			\
+  if(!GIVEN(list_name)							\
+     || isnt(scm_list_p(list_name))					\
+     || (scm_to_int(scm_length(list_name)) < 4)				\
+     ) {								\
+    glGetIntegerv(GL_VIEWPORT, (GLint *) &struct_name);			\
+  }									\
+  else {								\
+    SCM_LIST_TO_C_ARRAY(int, &struct_name, 4, scm_to_int, list_name);	\
+    struct_name.y = screen->h - struct_name.y - struct_name.h;		\
+  }
+
 static SCM
-unproject(SCM _x, SCM _y, SCM _z,
-	  SCM _modelview_matrix,
-	  SCM _projection_matrix,
-	  SCM _viewport_list) {
-  NOTE("this function is still experimental and needs to be tested thoroughly");
+world_to_screen_coordinates(SCM _x, SCM _y, SCM _z,
+			    SCM _modelview_matrix,
+			    SCM _projection_matrix,
+			    SCM _viewport_list) {
+  v3d m;
+  m.x = scm_to_double(_x);
+  m.y = scm_to_double(_y);
+  m.z = scm_to_double(_z);
+
+  m4x4d modelview_matrix = scm_to_m4x4d(_modelview_matrix);
+  m4x4d projection_matrix = scm_to_m4x4d(_projection_matrix);
+  struct { GLint x, y, w, h; } s;
+  VIEWPORT_LIST_TO_STRUCT(_viewport_list, s);
+  v3d v;
+  GLint result = gluProject(m.x, m.y, m.z, 
+			    (GLdouble *) &modelview_matrix,
+			    (GLdouble *) &projection_matrix,
+			    (GLint *) &s,
+			    &v.x, &v.y, &v.z);
+  if(result == GL_FALSE) {
+    return SCM_BOOL_F;
+  }
+  return scm_list_2(scm_from_int((int) v.x), 
+		    scm_from_int(screen->h - (int) v.y - 1));
+}
+			    
+
+static SCM
+screen_to_world_coordinates(SCM _x, SCM _y, SCM _z,
+			    SCM _modelview_matrix,
+			    SCM _projection_matrix,
+			    SCM _viewport_list) {
   v3d m;
   m.x = scm_to_double(_x);
   m.y = scm_to_double(_y);
@@ -315,17 +354,7 @@ unproject(SCM _x, SCM _y, SCM _z,
   m4x4d modelview_matrix = scm_to_m4x4d(_modelview_matrix);
   m4x4d projection_matrix = scm_to_m4x4d(_projection_matrix);
   struct { GLint x, y, w, h; } s;
-  if(!GIVEN(_viewport_list) 
-     || isnt(scm_list_p(_viewport_list))
-     || (scm_to_int(scm_length(_viewport_list)) < 4)
-     ) {
-    glGetIntegerv(GL_VIEWPORT, (GLint *) &s);
-  }
-  else {
-    SCM_LIST_TO_C_ARRAY(int, &s, 4, scm_to_int, _viewport_list);
-    s.y = screen->h - s.y - s.h;
-  }
-
+  VIEWPORT_LIST_TO_STRUCT(_viewport_list, s);
   v3d v;
   GLint result = gluUnProject(m.x, screen->h - m.y - 1, m.z, 
 			      (GLdouble *) &modelview_matrix,
@@ -333,12 +362,12 @@ unproject(SCM _x, SCM _y, SCM _z,
 			      (GLint *) &s,
 			      &v.x, &v.y, &v.z);
   if(result == GL_FALSE) {
-    // właściwie to powinniśmy raczej zasygnalizować jakiś błąd
     return SCM_BOOL_F;
-  }
-  
+  }  
   return scm_from_v3d(v);
 }
+
+#undef VIEWPORT_LIST_TO_STRUCT
 
 // by convention, each function that ever
 // alters a matrix mode, should leave GL_MODELVIEW
@@ -925,7 +954,11 @@ export_symbols(void *unused) {
   EXPORT_PROCEDURE("current-viewport", 0, 0, 0, current_viewport);
   EXPORT_PROCEDURE("current-projection", 0, 0, 0, 
 		   current_projection);
-  EXPORT_PROCEDURE("unproject", 5, 1, 0, unproject);
+  EXPORT_PROCEDURE("screen->world/coordinates", 5, 1, 0, 
+		   screen_to_world_coordinates);
+
+  EXPORT_PROCEDURE("world->screen/coordinates", 5, 1, 0, 
+		   world_to_screen_coordinates);
 
   EXPORT_PROCEDURE("quaternion->matrix", 1, 0, 0, quaternion_to_matrix);
   EXPORT_PROCEDURE("translation-matrix", 1, 0, 0, translation_matrix);
