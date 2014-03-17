66546d601ec614d5422b20d6409bcd4c881a2000
Maciek Godek <godek.maciek@gmail.com>
2014-03-17 23:22:09 +0100

    Added an editor for various parameters

--- demos/red.scm
+++ demos/red.scm
@@ -1,4 +1,4 @@
-#!../src/slayer -e3d
+#!../src/slayer -w 900
 exit
 !#
 
@@ -31,7 +31,9 @@ exit
 ;;   więzów
 ;; - zmiana parametrów więzów
 
-(define-class <physical-body> (<3d-object>)
+(define-class <editable-object> (<3d-object>))
+
+(define-class <physical-body> (<editable-object>)
   (mesh-cache #:allocation #:class #:init-value #[])
   (default-dimensions #:allocation #:class
     #:init-value '((sphere (radius . 0.5))
@@ -43,6 +45,9 @@ exit
    `((sphere . ,generate-sphere)
      (cylinder . ,generate-tube)
      (box . ,generate-box)))
+  (dimension-editors 
+   #:allocation #:class
+   #:init-value '()) ;; initalized elsewhere
   (shapes
    #:allocation #:virtual
    #:slot-ref (lambda(self)(map first #[self 'generators]))
@@ -57,7 +62,7 @@ exit
    #:slot-set!
    (lambda (self shape)
      (set! #[self '%shape] shape)
-     (set! #[self 'dimensions] #[self : 'default-dimensions : shape])))
+     (set! #[self 'dimensions] (copy-tree #[self : 'default-dimensions : shape]))))
   (mass #:init-value 1.0 #:init-keyword #:mass)
   (center-of-mass #:init-value #f32(0 0 0) #:init-keyword #:center-of-mass)
   (inertia-tensor #:init-value #2f32((1 0 0)
@@ -74,67 +79,127 @@ exit
 			      (append-map (lambda ((name . value))
 					    `(,(symbol->keyword name) ,value))
 				   dimensions))))
-	     (set! #[self : 'mesh-cache : `(,shape ,@dimensions)] mesh)
+	     (format (current-error-port)"new mesh: ~a\n"`(,shape ,@dimensions))
+	     (set! #[self : 'mesh-cache : (copy-tree `(,shape ,@dimensions))] mesh)
 	     mesh))))
    #:slot-set! noop))
-
-
-#;(parameter-editor  '((shape #:one-of ())
-		     (dimensions #:dynamic (shape))
-		     mass
-		     center-of-mass
-		     inertia-tensor))
-
-
-
-#|
-|#
-
 
 (define-method (initialize (self <physical-body>) args)
   (next-method)
   (if (not #[self 'dimensions])
       (set! #[self 'dimensions]
-	    #[self : 'default-dimensions : #[self 'shape]])))
+	    (copy-tree #[self : 'default-dimensions : #[self 'shape]]))))
 
-(define-method (set-body-shape! (body <physical-body>) (shape <symbol>) . args)
+(define-method (set-body-shape! #;of (body <physical-body>) 
+				     #;to (shape <symbol>) . args)
   (set! #[body 'shape] shape)
-  (set! #[body 'dimensions] 
-	(replace-alist-bindings #;in #[body 'dimensions] 
-				     #;with (keyword-args->alist args))))
-
-;;(define-method (set-previous-body-shape! (body <physical-body>))
-
-#;(define-class <joint> (<3d-object>)
-  ...)
+  (set! #[body 'dimensions]
+	(replace-alist-bindings #;in #[body 'dimensions]
+				     #;with (keyword-args->alist args)))
+  (set! #[the-dimension-editor 'content]
+	#[body : 'dimension-editors : shape])
+  (set-target! #;of the-dimension-editor #;as body))
 
 (define the-rig (make <3d-stage>))
 
-(add-object! (make <physical-body>) #;to the-rig)
+(publish
+ ;; tu jest -- w istocie, zgodzię się -- za dużo pisaniny!
+ ;; edytor powinien budować się sam na podstawie opisu obiektu
+ (define body-editor
+   ((layout #:x 640 #:y 20)
+    position-editor 
+    orientation-editor
+    mass-editor
+    inertia-tensor-1
+    inertia-tensor-2
+    inertia-tensor-3
+    dimension-editor))
+ (define the-dimension-editor dimension-editor)
+ where
+ (define body (make <physical-body>))
+ (define position-editor
+   (parameter-editor 
+    body
+    (" x: " #[body : 'position : 0])
+    (" y: " #[body : 'position : 1])
+    (" z: " #[body : 'position : 2])))
+ (define orientation-editor
+   (parameter-editor
+    body
+    (" s: " (head #[body 'orientation]))
+    (" i: " #[(tail #[body 'orientation]) 0])
+    (" j: " #[(tail #[body 'orientation]) 1])
+    (" k: " #[(tail #[body 'orientation]) 2])))
+ (define mass-editor
+   (parameter-editor
+    body
+    ("mass: " #[body 'mass])
+    (" x: " #[body : 'center-of-mass : 0])
+    (" y: " #[body : 'center-of-mass : 1])
+    (" z: " #[body : 'center-of-mass : 2])))
+ (define inertia-tensor-1 
+   (parameter-editor
+    body 
+    ("t" #[#[body 'inertia-tensor] 0 0])
+    ("e" #[#[body 'inertia-tensor] 0 1])
+    ("n" #[#[body 'inertia-tensor] 0 2])))
+ (define inertia-tensor-2 
+   (parameter-editor
+    body 
+    ("s" #[#[body 'inertia-tensor] 1 0])
+    ("o" #[#[body 'inertia-tensor] 1 1])
+    ("r" #[#[body 'inertia-tensor] 1 2])))
+ (define inertia-tensor-3
+   (parameter-editor
+    body 
+    ("i" #[#[body 'inertia-tensor] 2 0])
+    ("n" #[#[body 'inertia-tensor] 2 1])
+    ("e" #[#[body 'inertia-tensor] 2 2])))
+ (define dimension-editor
+   (make <container-widget> #:min-h 12))
+ (add-object! body #;to the-rig)
+ (keydn 'v (lambda ()
+	     (<< #[body 'dimensions])))
+ (set! #[body 'dimension-editors]
+       (map (lambda ((name . parameters))
+	      `(,name
+		.
+		,(apply 
+		  (layout #:lay-out lay-out-horizontally)
+		  (map (lambda ((param . _))
+			 (make <numeric-input> #:w 80 #:h 12
+			       #:label 
+			       (string-append (symbol->string param) ": ")
+			       #:accessor
+			       (accessor x #[x : 'dimensions : param])))
+		       parameters))))
+	    #[body 'default-dimensions])))
+
+(add-child! body-editor #;to *stage*)
+
+
+(define-method (select-object! (o <editable-object>) #;from (v <3d-editor>))
+  (next-method)
+  (set! #[the-dimension-editor 'content]
+	#[o : 'dimension-editors : #[o 'shape]])
+  (set-target! #;of body-editor #;as o))
 
 (define view (make <3d-editor> 
 	       #:x 10 #:y 10 
-	       #:w (- (screen-width) 20) 
-	       #:h (- (screen-height) 20)
+	       #:w 620
+	       #:h 460
 	       #:stage the-rig))
-
-(set-screen-size! 800 480)
 
 (add-child! view #;to *stage*)
 
-#|
-(add-child! 
- (layout:
-  )   
- #;to *stage*)
-|#
+;;(<< #[*stage* 'x] ", " #[*stage* 'y] ", " #[*stage* 'w] ", " #[*stage* 'h])
 
 (set! #[view 'left-click]
       (lambda (x y)
 	(unselect-all! view)
 	(let ((object (object-at-position x y view)))
 	  (if object
-	      (select-object! view object)
+	      (select-object! object #;from view)
 	      (add-object! 
 	       (make <physical-body> #:position (screen->3d view x y 0.95))
 	       #;to the-rig)))))
