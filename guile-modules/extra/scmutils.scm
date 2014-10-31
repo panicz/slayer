37d2e0ba76847f4a597723645b662cd7dfa614e7
Maciek Godek <godek.maciek@gmail.com>
2014-10-31 18:06:52 +0100

    Adding the guile-scmutils package to the repository. It might be removed in the future, but it's better to keep it for now, as the singular-value decomposition might come in handy for inverse kinematics

---
+++ guile-modules/extra/scmutils.scm
@@ -0,0 +1,8 @@
+(define-module (extra scmutils)
+  #:use-module (extra common)
+  #:use-module (extra math)
+  #:export (svd array->matrix))
+
+(load "scmutils/load.scm")
+
+(set-current-module generic-environment)
