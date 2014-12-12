e282aa2942b1d9ba207eaf3005ad51587a72947b
Maciek Godek <godek.maciek@gmail.com>
2014-12-12 19:41:26 +0100

    Increasing the export list to support the "pseudoinverse" function from (extra math)

--- guile-modules/extra/scmutils.scm
+++ guile-modules/extra/scmutils.scm
@@ -1,7 +1,7 @@
 (define-module (extra scmutils)
   #:use-module (extra common)
   #:use-module (extra math)
-  #:export (svd array->matrix))
+  #:export (svd svd-invert array->matrix matrix->array))
 
 (load "scmutils/load.scm")
 
