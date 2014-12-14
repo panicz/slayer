4b53cda44dbf9a566b480a366b5cb46750c8c2d7
Maciek Godek <godek.maciek@gmail.com>
2014-12-14 01:25:46 +0100

    Adding the first (broken) version of inverse kinematics. Although it definitely requires some further work, it is elaborated enough to be the part of the repository, so that any pending hypotheses can be reverted at will

--- demos/editor/relations.scm
+++ demos/editor/relations.scm
@@ -1,5 +1,6 @@
 (define-module (editor relations)
   #:use-module (extra common)
+  #:use-module (extra math)
   #:export (
 	     bodies-linked-to
 	     split-bodies-at
@@ -7,8 +8,17 @@
 	     joint-connecting-bodies
 	     body-attached-by
 	     joints-attached-to
-	     kinematic-chains
-	    ))
+	     body-sequences
+	     body-island-leaves
+	     shortest-joint-sequence-from+furthest-end
+	     shortest-joint-sequence
+
+	     anchors+orientations<-kinematic-chain
+	     kinematic-chain<-anchors+orientations
+	     kinematic-chain-substitute
+	     position<-angles
+	     desired-configuration
+	     ))
 
 ;; Throughout this module, we understand that two bodies are ATTACHED
 ;; to each other if there exists a joint that connects those bodies
@@ -52,7 +62,7 @@
 	       attached-bodies))))
 
 (define (split-bodies-at joint)
-  (match-let (((left-body right-body) (two-bodies-attached-by joint)))
+  (let (((left-body right-body) (two-bodies-attached-by joint)))
     (values
      (union (bodies-linked-to left-body #:except `(,right-body))
 	    `(,left-body))
@@ -71,12 +81,23 @@
   (not (null? (intersection (joints-attached-to body-1)
 			    (joints-attached-to body-2)))))
 
-(define (body-island-leaves #;of body-island)
-  (filter (lambda (body)
-	    (= (length (joints-attached-to body)) 1))
-	  body-island))
+(define (body-island? l)
+  (match l
+    ((first . rest)
+     (let ((island (bodies-linked-to first)))
+       (for-every body in rest
+	 (in? body island))))))
 
-(define (kinematic-chains #:from start #:to end #:excluding (bodies '()))
+(define (body-island-leaves body-island)
+  (filter (lambda (body)
+	    (let ((joints (joints-attached-to body)))
+	      (or (= (length joints) 1)
+		  (apply equal? (map (lambda (joint)
+				       (body-attached-by joint #;to body))
+				     joints)))))
+   body-island))
+
+(define (body-sequences #:from start #:to end #:excluding (bodies '()))
   (let ((neighbours (difference (bodies-attached-to start) `(,start) bodies)))
     (cond ((or (null? neighbours) (in? start bodies))
 	   #f)
@@ -86,7 +107,7 @@
 	   (let ((subchains (concatenate
 			     (filter-map
 			      (lambda (x)
-				(kinematic-chains 
+				(body-sequences 
 				 #:from x #:to end
 				 #:excluding `(,start ,@bodies)))
 			      neighbours))))
@@ -94,3 +115,122 @@
 		  (map (lambda (subchain)
 			 `(,start ,@subchain))
 		       subchains)))))))
+
+(define (shortest-joint-sequence #;from start #;to end)
+  (let ((bodies (apply argmin length (body-sequences #;from start #;to end))))
+    (map joint-connecting-bodies (drop-right bodies 1) (drop bodies 1))))
+
+(define (shortest-joint-sequence-from+furthest-end #;to body)
+  (let* ((island (bodies-linked-to body))
+	 (ends (delete body #;from (body-island-leaves island)))
+	 (sequence-sets (map (lambda (end)
+			       (body-sequences #:from body #:to end))
+			     ends))
+	 (set-of-sequences-from-body-to-furthest-end 
+	  (apply argmax (lambda (sequences) (apply min (map length sequences)))
+		 sequence-sets))
+	 (shortest-body-sequence-from-furthest-end-to-body
+	  (reverse (apply argmin length 
+			  set-of-sequences-from-body-to-furthest-end)))
+	 (corresponding-joint-sequence
+	  (map joint-connecting-bodies
+	       (drop-right shortest-body-sequence-from-furthest-end-to-body 1)
+	       (drop shortest-body-sequence-from-furthest-end-to-body 1))))
+    (values corresponding-joint-sequence
+	    (first shortest-body-sequence-from-furthest-end-to-body))))
+
+(publish
+ (define (kinematic-chain<-anchors+orientations anchors+rotations)
+   (local<-global anchors+rotations #f32(0 0 0) '(1.0 . #f32(0 0 0))))
+ where 
+ (define (local<-global global-anchor+rotation-chain translation rotation)
+   (match global-anchor+rotation-chain
+     (()
+      '())
+     (((global-anchor global-rotation) . rest)
+      (let ((new-rotation (* rotation global-rotation))
+	    (new-anchor (rotate (- global-anchor translation)
+				#;by rotation)))
+	`((,new-anchor ,new-rotation) 
+	  . ,(local<-global rest global-anchor 
+			    (* (~ new-rotation) rotation))))))))
+
+(publish
+ (define (anchors+orientations<-kinematic-chain chain)
+   (global<-local chain #f32(0 0 0) '(1.0 . #f32(0 0 0))))
+ where
+ (define (global<-local kinematic-chain translation rotation)
+   (match kinematic-chain
+     (()
+      '())
+     (((local-anchor local-rotation) . rest)
+      (let ((new-anchor (+ translation (rotate local-anchor #;by rotation)))
+	    (new-rotation (* rotation local-rotation)))
+      `((,new-anchor ,new-rotation) . ,(global<-local rest new-anchor
+						      new-rotation)))))))
+
+(assert (and (identity? (compose anchors+orientations<-kinematic-chain
+				 kinematic-chain<-anchors+orientations))
+	     (identity? (compose kinematic-chain<-anchors+orientations
+				 anchors+orientations<-kinematic-chain))))
+
+(publish
+ (define (kinematic-chain-substitute kinematic-chain)
+   "a single translation and rotation that stand for the application of\
+ the whole kinematic chain"
+   (translation+rotation kinematic-chain #f32(0 0 0) '(1.0 . #f32(0 0 0))))
+ where
+ (define (translation+rotation local-chain translation rotation)
+   (match local-chain
+     (()
+      (values translation rotation))
+     (((local-anchor local-rotation) . rest)
+      (translation+rotation rest (+ translation (rotate local-anchor
+							#;by rotation))
+			    (* rotation local-rotation))))))
+
+;; co nam powinna dawać funkcja kinematyki odwrotnej?
+;; co powinna pobierać?
+;; powinno to być coś takiego, co -- dla danych punktów początkowego i
+;; końcowego oraz początkowej konfiguracji kątów zwraca nową konfigurację
+;; kątów
+
+(define (desired-configuration initial-position desired-position
+			       initial-configuration system-equation)
+  ;; inverse kinematics routine.
+  ;; initial and desired positions are expressed in global coordinate system.
+  ;; 
+  (assert (and (uniform-vector? initial-position)
+	       (uniform-vector? desired-position)
+	       (list? initial-configuration)
+	       (list? (desired-configuration 
+		       initial-position desired-position 
+		       initial-configuration system-equation))))
+  (let ((position-increment (- desired-position initial-position))
+	(jacobian (apply ((isotropic-jacobian-approximation 
+			   #;of (compose uniform-vector->list system-equation))
+			  #;by 0.0001)
+			 #;at initial-configuration)))
+    (map + initial-configuration 
+	 (uniform-vector->list
+	  (* (pseudoinverse #;of jacobian) position-increment)))))
+
+(define (position<-angles global-anchors+axes local-position)
+  (impose-arity
+   `(,(length global-anchors+axes) 0 #f)
+   (lambda angles
+     (let* ((anchors+orientations (map (lambda ((anchor axis) angle)
+					 `(,anchor ,(rotation-quaternion 
+						     #;around axis 
+							      #;by angle)))
+				       global-anchors+axes angles))
+	    (kinematic-chain (kinematic-chain<-anchors+orientations
+			      anchors+orientations))
+	    (translation rotation 
+			 (kinematic-chain-substitute kinematic-chain)))
+       (+ translation (rotate local-position #;by rotation))))))
+
+(define (inverse-translation+rotation translation rotation)
+  (let ((inverse (~ rotation)))
+    (values (rotate (- translation) #;by inverse)
+	    inverse)))
--- demos/posed.scm
+++ demos/posed.scm
@@ -16,9 +16,11 @@ exit
 	     (widgets 3d)
 	     (editor relations)
 	     (editor poses)
+	     (extra scmutils)
 	     (scum physics))
 
 (set-window-title! "POSED: The POSE Editor")
+
 
 (define the-simulation (primitive-make-simulation))
 
@@ -56,12 +58,19 @@ exit
     rig-angles))
 
 (define (current-pose)
+  "the desired pose currently set up"
   `(pose ,@(hash-map->list cons rig-angles)))
 
-(define (joint-angles rig)
-  `(pose ,@(map (lambda(joint)
-		  `(,(joint-name joint) . ,(joint-property joint 'angle)))
-		(rig-joints rig))))
+(define ((rig-joints-name&property-getter property) rig)
+  "the properties are obtained directly from ODE simulator"
+  (map (lambda (joint) `(,(joint-name joint) ,(joint-property joint property)))
+       (rig-joints rig)))
+
+(define joint-angles (rig-joints-name&property-getter 'angle))
+
+(define joint-axes (rig-joints-name&property-getter 'axis))
+
+(define joint-anchors (rig-joints-name&property-getter 'anchor))
 
 (define (rotate-body! body #;by angle #;around axis #;at pivot)
   (let ((position (body-property body 'position))
@@ -70,35 +79,33 @@ exit
 			(rotate position #;by angle #;around axis
 				#;at pivot))
     (set-body-property! body 'quaternion
-			(* (rotation-quaternion #;around axis
-							 #;by angle)
+			(* (rotation-quaternion #;around axis #;by angle)
 			   orientation))))
 
-(define* (set-pose! #;of rig #;to pose 
-			 #:key (keeping (match pose (('pose (first . _) . _)
-						     first))))
+(define* (set-pose! #;of rig #;to pose #:key (keeping #f))
   (assert (pose? pose))
   (with-context-for-joint/body-relation
-   (match pose
-     (('pose . parameters)
-      (for (joint-name . angle) in parameters
-	(when angle
-	  (let*-values (((joint) (joint-named joint-name #;from rig))
-			((mobile-bodies immobile-bodies)
-			 (split-bodies-at joint))
-			((angle*) (- angle #[rig-angles joint-name]))
-			((mobile-joints) 
-			 (unique (fold union '()
-				       (map joints-attached-to
-					    mobile-bodies))))
-			((pivot axis) 
-			 (begin
-			   (assert (eq? (joint-type joint) 'hinge))
-			   (values (joint-property joint 'anchor)
-				   (joint-property joint 'axis)))))
-	    (set! #[rig-angles joint-name] angle)
-	    (for body in mobile-bodies
-	      (rotate-body! body #;by angle* #;around axis #;at pivot)))))))))
+   (let ((('pose . parameters) pose)
+	 (fixed keeping))
+     (for (joint-name . angle) in parameters
+       (when angle
+	 (let* ((joint (joint-named joint-name #;from rig))
+		(mobile-bodies immobile-bodies 
+			       (let ((left right (split-bodies-at joint)))
+				 (if (and keeping (in? keeping left))
+				     (values right left)
+				     (values left right))))
+		(angle* (- angle #[rig-angles joint-name]))
+		(mobile-joints (unique (fold union '()
+					     (map joints-attached-to
+						  mobile-bodies))))
+		(pivot axis (begin
+			      (assert (eq? (joint-type joint) 'hinge))
+			      (values (joint-property joint 'anchor)
+				      (joint-property joint 'axis)))))
+	   (set! #[rig-angles joint-name] angle)
+	   (for body in mobile-bodies
+	     (rotate-body! body #;by angle* #;around axis #;at pivot))))))))
 
 (include "temporary-poses.scm")
 
@@ -111,17 +118,88 @@ exit
 (keydn 'esc (lambda () (unselect-all! view)))
 
 (define-method (select-body! body #;from (view <3d-view>))
-  (let ((object (find (lambda(x)
-			(and (is-a? x <physical-object>) 
-			     (equal? #[x 'body] body)))
-		      #[view : 'stage : 'objects])))
-    (if object
-	(select-object! object #;from view)
-	(format #t "no body found in ~s\n" #[view : 'stage : 'objects]))))    
+  (and-let* ((object (find (lambda(x)
+			     (and (is-a? x <physical-object>) 
+				  (equal? #[x 'body] body)))
+			   #[view : 'stage : 'objects])))
+    (select-object! object #;from view)
+    (format #t "no body found in ~s\n" #[view : 'stage : 'objects])))
+
+(keydn 'i
+  (lambda ()
+    (with-context-for-joint/body-relation    
+     (unless (null? #[view 'selected])
+       (let* ((body #[(element #[view 'selected]) 'body])
+	      (joint-sequence
+	       (shortest-joint-sequence-from-furthest-end #;to body)))
+	 ;; (assert (apply eq? 'hinge (map joint-type joint-sequence)))
+	 (pretty-print (map joint-name joint-sequence)))))))
+
+(define* (apply-inverse-kinematics! #;of selected-body #;to position 
+					#:optional #;anchored-at fixed)
+  (let* ((joint-sequence fixed (if fixed
+				   (values (shortest-joint-sequence 
+					    #;from fixed #;to selected-body)
+					   fixed)
+				   (shortest-joint-sequence-from+furthest-end 
+				    #;to selected-body)))
+	 (((anchors angles axes) ...)
+	  (map (lambda (joint) (map (lambda (property)
+				 (joint-property joint property))
+			       '(anchor angle axis)))
+	       joint-sequence))
+	 (anchors+orientations (map (lambda(anchor angle axis)
+				      `(,anchor ,(rotation-quaternion
+						  #;around axis
+							   #;by angle)))
+				    anchors angles axes))
+	 (kinematic-chain (kinematic-chain<-anchors+orientations
+			   anchors+orientations))
+	 (translation rotation (kinematic-chain-substitute kinematic-chain))
+	 (global-body-position (body-property selected-body 'position))
+	 (local-body-position (rotate (- global-body-position translation)
+				      #;by (~ rotation)))
+	 (system-equation (position<-angles (map list anchors axes)
+					    local-body-position))
+	 (angles (desired-configuration global-body-position
+					position angles
+					system-equation))
+	 (pose `(pose . ,(map (lambda(joint angle)
+				`(,(joint-name joint) . ,angle))
+			      joint-sequence angles)))
+	 )
+    (assert (apply eq? 'hinge (map joint-type joint-sequence)))
+    (set-pose! #;of the-legs #;to pose #:keeping fixed)))
+
+(define ((ik-mode view))
+  (unless (null? #[view 'selected])
+    (let* ((old-bindings (current-key-bindings))
+	   (final-object (first #[view 'selected]))
+	   (final-body #[final-object 'body])
+	   (initial-position #[final-object 'position])
+	   ((_ _ zs) (3d->screen view initial-position)))
+      (set-key-bindings!
+       (key-bindings
+	(keydn 'esc
+	  (lambda ()
+	    ;; the following method is inaccurate and probably won't restore
+	    ;; the configuration to the initial setting. To implement this
+	    ;; accurately, we'd need to store the angles of subsequent hinges,
+	    ;; but this is done internally by `apply-inverse-kinematics!'
+	    (apply-inverse-kinematics! #;of final-body #;to initial-position)
+	    (set-key-bindings! old-bindings)))
+	(mousemove
+	 (lambda (x y xrel yrel)
+	   (with-context-for-joint/body-relation
+	    (apply-inverse-kinematics!
+	     #;of final-body #;to (screen->3d view x y zs))))))))))
+
+(keydn 'k (ik-mode view))
 
 (keydn 'g (grab-mode view))
 
 (define pause #f)
+
 (keydn 'p (lambda () (set! pause (not pause))))
 
 (define ((rotate-around-joint/mode view))
@@ -130,13 +208,13 @@ exit
      ((first second)
       (let ((joint (joint-connecting-bodies first second))
 	    (previously-selected #[view 'selected]))
-	(let*-values (((left right) (split-bodies-at joint))
-		      ((move still) (cond ((in? first left)
-					   (values left right))
-					  ((in? first right)
-					   (values right left))
-					  (else
-					   (error)))))
+	(let* ((left right (split-bodies-at joint))
+	       (move still (cond ((in? first left)
+				  (values left right))
+				 ((in? first right)
+				  (values right left))
+				 (else
+				  (error)))))
 	  (unselect-all! #;from view)
 	  (for body in move
 	    (select-body! body #;from view))
@@ -157,13 +235,18 @@ exit
      (else
       (display "exactly two objects need to be selected\n")))))
 
+(define-syntax-rule (gimme shit)
+  (lambda () (pretty-print shit #:width 160)))
+
 (keydn 'h (rotate-around-joint/mode view))
 
-(keydn 'c (lambda ()
-	    (pretty-print (current-pose))))
+(keydn 'c (gimme (current-pose)))
 
-(keydn 'b (lambda ()
-	    (pretty-print (joint-angles the-legs))))
+(keydn 'b (gimme (joint-angles the-legs)))
+
+(keydn 'x (gimme (joint-axes the-legs)))
+
+(keydn 'z (gimme (joint-anchors the-legs)))
 
 (keydn 'v (lambda ()
 	    (set-pose! #;of the-legs #;to (current-pose))))
