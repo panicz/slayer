f1136422cb84d6896b0724c6ea1320d9a41ae645
Maciek Godek <godek.maciek@gmail.com>
2014-12-30 13:56:00 +0100

    Removed (although sloppily) a few redundant and broken procedures

--- demos/editor/modes.scm
+++ demos/editor/modes.scm
@@ -13,11 +13,19 @@
 	     desired-configuration
 	     position-at-tip-of-kinematic-chain
 	     global-positions
+	     global-positions<-kinematic-chain
 	    ))
 
 (publish
  (define (kinematic-chain<-anchors+axes+angles anchors axes angles)
    (kinematic-chain (zip anchors axes angles)))
+
+ (define (build-kinematic-chain #;from anchors+axes #;and angles)
+   (map (lambda ((anchor axis angle))
+	  `(,anchor ,(rotation-quaternion #;around axis #;by angle)))
+	(kinematic-chain (map (lambda ((anchor axis) angle)
+				`(,anchor ,axis ,angle))
+			      anchors+axes angles))))
  where
  (define (kinematic-chain global-sequence)
    (match global-sequence
@@ -36,13 +44,6 @@
 (publish
  (define (kinematic-chain<-anchors+orientations anchors+rotations)
    (local<-global anchors+rotations #f32(0 0 0) '(1.0 . #f32(0 0 0))))
- (define (build-kinematic-chain #;from anchors+axes #;and angles)
-   (let ((anchors+orientations (map (lambda ((anchor axis) angle)
-				      `(,anchor ,(rotation-quaternion
-						  #;around axis
-							   #;by angle)))
-				    anchors+axes angles)))
-     (kinematic-chain<-anchors+orientations anchors+orientations)))
  where 
  (define (local<-global global-anchor+rotation-chain translation rotation)
    (match global-anchor+rotation-chain
@@ -55,6 +56,20 @@
 	`((,new-anchor ,new-rotation) 
 	  . ,(local<-global rest global-anchor 
 			    (* (~ new-rotation) rotation))))))))
+
+(define (global-positions<-kinematic-chain kinematic-chain)
+  (match kinematic-chain
+    (()
+     '())
+    (((position rotation) . rest)
+     `(,position ,@(global-positions<-kinematic-chain
+		    (map (lambda ((dependent-position dependent-rotation))
+			   `(,(rotate (+ dependent-position  position)
+					 #;by rotation)
+			     (* rotation dependent-rotation #;rotation)))
+			 rest))))
+    (else
+     kinematic-chain)))
 
 (publish
  (define (global-positions kinematic-chain)
@@ -75,7 +90,8 @@
       '())
      (((local-anchor local-rotation) . rest)
       (let ((new-anchor (+ translation (rotate local-anchor #;by rotation)))
-	    (new-rotation (rotate rotation #;by local-rotation)))
+	    (new-rotation (* rotation local-rotation)
+			  ))
       `((,new-anchor ,new-rotation) . ,(global<-local rest new-anchor
 						      new-rotation)))))))
 
@@ -95,9 +111,17 @@
      (()
       (values translation rotation))
      (((local-anchor local-rotation) . rest)
-      (translation+rotation rest (+ translation (rotate local-anchor
-							#;by rotation))
-			    (* rotation local-rotation))))))
+      (let ((new-rotation (rotate rotation (~ local-rotation))))
+	(translation+rotation rest (+ translation (rotate local-anchor
+							  #;by (~ rotation)))
+			      new-rotation))))))
+
+;; co musimy przetestować? jakie ten nasz kod czyni założenia?
+;; - mamy daną funkcję, która pobiera kąty i zwraca położenie
+;; elementu końcowego. W jakim układzie współrzędnych wyrażone
+;; jest to położenie?
+;; - skąd bierze się taka jednorodność jakobianu? czy to aby
+;; nie podejrzane?
 
 (define (position-at-tip-of-kinematic-chain kinematic-chain local-position)
   (match kinematic-chain
@@ -109,8 +133,19 @@
 					    (rotate local-position 
 						    #;by local-rotation))))))
 
+(define (position<-angles global-anchors+axes local-position)
+  (let ((N (length global-anchors+axes)))
+    (impose-arity
+     `(,N 0 #f)
+     (lambda angles
+       (let ((kinematic-chain (build-kinematic-chain global-anchors+axes
+						     angles)))
+	 (position-at-tip-of-kinematic-chain kinematic-chain 
+					     local-position))))))
+
 (define (desired-configuration initial-position desired-position
-			       initial-configuration system-equation)
+			       initial-configuration system-equation
+			       direction-correction)
   ;; inverse kinematics routine.
   ;; initial and desired positions are expressed in global coordinate system.
   ;; 
@@ -121,31 +156,30 @@
 		       initial-position desired-position 
 		       initial-configuration system-equation))))
   (let* ((position-increment (- desired-position initial-position))
-	 (increment-magnitude (norm position-increment))
+	 ;;(increment-magnitude (norm position-increment))
 	 (jacobian (apply ((isotropic-jacobian-approximation 
-			    #;of (compose uniform-vector->list system-equation))
-			   #;by 0.0002)
-			  #;at initial-configuration)))
-    (pretty-print (!# initial-position))
-    ;(pretty-print jacobian)
-    (map + initial-configuration 
-	 (uniform-vector->list
-	  (* (pseudoinverse #;of jacobian) position-increment)))))
-
-(define (position<-angles global-anchors+axes local-position)
-  #;(pretty-print (list (!# global-anchors+axes)
-		      (!# local-position)))
-  (let ((N (length global-anchors+axes)))
-    (impose-arity
-     `(,N 0 #f)
-     (lambda angles
-       (let* (;;(angles (reverse angles))
-	      ;;(alter #;element-number (- N 1) #;in angle #;with 0.0)
-	      (kinematic-chain (build-kinematic-chain global-anchors+axes
-						      angles))
-	      (translation rotation 
-			   (kinematic-chain-substitute kinematic-chain)))
-	 (+ translation (rotate local-position #;by rotation)))))))
+			    #;of (compose uniform-vector->list 
+					  system-equation))
+			   #;by 0.00001)
+			  #;at initial-configuration))
+	 (jacobian+ (pseudoinverse #;of jacobian))
+	 (angle-increment (uniform-vector->list 
+			   (* jacobian+ position-increment)))
+	 (result (map * #;direction-correction
+		      (map normalized-radians 
+			   (map + initial-configuration angle-increment))))
+	 (obtained-position (apply system-equation #;to result)))
+    ;;(<<< (* (list->uniform-array jacobian) jacobian+))
+    (<<< direction-correction desired-position obtained-position result)
+    #;(<< 
+     "initial: " initial-position
+	"desired: " desired-position
+	"obtained: " obtained-position
+	"desired increment: " position-increment
+	"difference: " (norm (- desired-position obtained-position))
+	"angle increment: " angle-increment
+	)
+    result))
 
 (define (inverse-translation+rotation translation rotation)
   (let ((inverse (~ rotation)))
