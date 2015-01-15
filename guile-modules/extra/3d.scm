db28aa1f99c3a882d0ea25b3fcb1c86bbe59aabb
Maciek Godek <godek.maciek@gmail.com>
2015-01-15 18:21:51 +0100

    Adding 'coordinate-system-mesh' object

--- guile-modules/extra/3d.scm
+++ guile-modules/extra/3d.scm
@@ -5,8 +5,6 @@
   :use-module (extra math)
   :use-module (slayer)
   :use-module (slayer 3d)
-  ;;:use-module (extra shape)
-  ;;:duplicates (warn merge-generics); replace warn-override-core warn last)
   :export (
 	   <3d> 
 	   <3d-cam> 
@@ -23,6 +21,7 @@
 	   skip-lights
 	   transform-mesh-vertices
 	   x-axis y-axis z-axis
+	   coordinate-system-mesh
 	   )
   :export-syntax (replace/mesh)
   ;;:re-export (distance)
@@ -32,38 +31,12 @@
 (define y-axis #f32(0 1 0))
 (define z-axis #f32(0 0 1))
 
-;(use-modules (extra common) (extra math))
-
-
-;; #;(describe (normals/triangles array? list?) array?)
-
-;; #;(expect 
-;;  (equal? (normals/triangles #2f32((0 0 1) (0 1 0) (0 -1 0)) '(0 1 2))
-;; 	 #2f32((-1.0 -0.0 -0.0)(-1.0 -0.0 -0.0)(-1.0 -0.0 -0.0))))
-
-;; (define (normals/triangles vertices triangle-indices/list)
-;;   (let* ((3d-list (match-lambda ((x y)       (list x y .0))
-;; 		                ((x y z . _) (list x y z))))
-;; 	 (vertices (case (array-rank vertices)
-;; 		     ((1)  (map-n 3 list (array->list vertices)))
-;; 		     ((2)  (map 3d-list (array->list vertices)))
-;; 		     (else (throw 'unsupported-array-rank vertices))))
-;; 	 (normals (make-vector (length vertices) '())))
-;;     (for indices in (map-n 3 list triangle-indices/list)
-;; 	 (match-let (((v1 v2 v3) 
-;; 		      (map (lambda(i)(list-ref vertices i)) indices)))
-;; 	   (let ((normal (normalized (^ (- v2 v1) (- v3 v2)))))
-;; 	     (for i in indices
-;; 		  (vector-set! normals i 
-;; 			       (cons normal 
-;; 				     (vector-ref normals i)))))))
-;;     (for i in 0 .. (last-index normals)
-;; 	 (let ((vertex-normals (vector-ref normals i)))
-;; 	       (vector-set! normals i 
-;; 			    (if (null? vertex-normals)
-;; 				'(.0 .0 .0)
-;; 				(normalized (apply + vertex-normals))))))
-;;     (list->typed-array 'f32 2 (vector->list normals))))
+(define coordinate-system-mesh
+  '(mesh 
+    (vertices #2f32((0 0 0)(1 0 0)(0 1 0)(0 0 1)))
+    (colors #2f32((1 1 1)(1 0 0)(0 1 0)(0 0 1)))
+    (faces
+     (lines #2u8((0 1)(0 2)(0 3))))))
 
 ;; #;(transform (v0 v1 v2 v3 v4 ...) ((v0 v1 v2)(v2 v1 v3)(v2 v3 v4) ...))
 ;; #;(transform (v0 v1 v2 v3 v4 v5 v6 v7 ...)
@@ -71,144 +44,6 @@
 
 ;; #;(transform (v0 v1 v2 v3 v4 ...)
 ;; 	     ((v0 v1 v2)(v0 v2 v3)(v0 v3 v4) ...))
-
-;; (define (indices->list indices) 
-;;   (flatten (array->list indices)))
-
-;; (define* (triangle-strip->triangles indices/flat-list #:optional (swap #f))
-;;   (match indices/flat-list
-;;     ((v0 v1 v2 . _)
-;;      (cons (if swap (list v1 v0 v2) (list v0 v1 v2)) 
-;; 	   (triangle-strip->triangles (drop indices/flat-list 1) (not swap))))
-;;     (else '())))
-
-;; (define (triangle-fan->triangles indices/flat-list)
-;;   (match-let (((pivot . edges) indices/flat-list))
-;;     (map (lambda (second third) (list pivot second third))
-;; 	 (drop-right edges 1) (drop edges 1))))
-
-;; (define (quads->triangles indices/flat-list)
-;;   (match indices/flat-list
-;;     ((a b c d . rest)
-;;      (cons (list a b c) (cons (list c a d) (quads->triangles rest))))
-;;     (else
-;;      '())))
-
-;; (define (quad-strip->quads indices/flat-list)
-;;   (match indices/flat-list
-;;     ((a b c d . _)
-;;      (cons (list a b c d) (quad-strip->quads (drop indices/flat-list 2))))
-;;     (else
-;;      '())))
-
-;; (define (quad-strip->triangles indices/flat-list)
-;;   (quads->triangles (flatten (quad-strip->quads indices/flat-list))))
-
-;; (define (triangle-indices type indices/list)
-;;   (case type
-;;     ((triangles) (flatten indices/list))
-;;     ((triangle-strip) (triangle-strip->triangles (flatten indices/list)))
-;;     ((triangle-fan polygon) (triangle-fan->triangles (flatten indices/list)))
-;;     ((quads) (quads->triangles (flatten indices/list)))
-;;     ((quad-strip) (quad-strip->triangles (flatten indices/list)))))
-
-;; (define (split-into-display-units mesh-definition)
-;;   "Each display unit contains one ``vertices'' section and typically 
-;;  at least one ``faces'' section (besides some other stuff)"
-;;   (let loop ((definition mesh-definition)
-;; 	     (current-display-unit '())
-;; 	     (finished-display-units '())
-;; 	     (already-had-faces #f))
-;;     (match definition
-;;       (()
-;;        (reverse (cons `(,(reverse current-display-unit))
-;; 		      finished-display-units)))
-;;       ((((? symbol? symbol) args ...) . rest)
-;;        (if (and (eq? symbol 'vertices) already-had-faces)
-;; 	   (loop rest `((,symbol ,@args))
-;; 		 (cons `(,(reverse current-display-unit))
-;; 		       finished-display-units)
-;; 		 #t)
-;; 	   (loop rest (cons `(,symbol ,@args) current-display-unit)
-;; 		 finished-display-units 
-;; 		 (or already-had-faces (eq? symbol 'faces))))))))
-
-;; (define (insert-normals display-unit)
-;;   (and-let* ((vertices (filter-map (match-lambda 
-;; 				     (('vertices . vertices)
-;; 				      vertices)
-;; 				     (else #f))
-;; 				   display-unit))
-;; 	     ((= (length vertices) 1))
-;; 	     (vertices (first vertices))
-;; 	     ((<< "vertices: "vertices))
-;; 	     (faces (append-map (match-lambda ((type indices)
-;; 					       (triangle-indices 
-;; 						type 
-;; 						(indices->list indices))))
-;; 				(filter-map (match-lambda (('faces . faces) faces) 
-;; 					      (else #f))
-;; 					    display-unit)))
-;; 	     ((<<"faces: " faces))
-;; 	     (normals (list->uniform-array (normals/triangles vertices faces))))
-;;     (append-map (lambda (entry) (match entry
-;; 				  (('vertices . _)
-;; 				   (list entry `(normals ,normals)))
-;; 				  (else
-;; 				   (list entry))))
-;; 		display-unit)))
-
-;; (define (mesh-with-normals mesh)
-;;   (match mesh
-;;     (('mesh . definition)
-;;      (if (any (matches? ('normals . _)) definition)
-;; 	 mesh ; already has normals, so we don't want to interfere
-;; 	 `(mesh ,@(let ((display-units (split-into-display-units definition)))
-;; 		    (apply append
-;; 			   (append-map insert-normals
-;; 				       display-units))))))))
-
-
-;; (mesh-with-normals (generate-hemisphere #:slices 2 #:stacks 2))
-
-
-;; (mesh 
-;;  (vertices #2f32((-1.0 1.22460635382238e-16 0.0) 
-;; 		 (1.0 0.0 0.0) 
-;; 		 (-0.866025388240814 1.06054024092951e-16 0.5) 
-;; 		 (0.866025388240814 0.0 0.5) 
-;; 		 (0.0 0.0 1.0))) 
-;;  (colors #2f32((-1.0 1.22460635382238e-16 0.0) 
-;; 	       (1.0 0.0 0.0) 
-;; 	       (-0.866025388240814 1.06054024092951e-16 0.5) 
-;; 	       (0.866025388240814 0.0 0.5) 
-;; 	       (0.0 0.0 1.0))) 
-;;  (faces (quad-strip #u8(0 2 1 3 0 2)) (triangle-fan #u8(4 3 2 1 0))))
-
-;; ((((vertices #2f32((-1.0 1.22460635382238e-16 0.0) 
-;; 		   (1.0 0.0 0.0) 
-;; 		   (-0.866025388240814 1.06054024092951e-16 0.5) 
-;; 		   (0.866025388240814 0.0 0.5) (0.0 0.0 1.0))) 
-;;    (colors #2f32((-1.0 1.22460635382238e-16 0.0) (1.0 0.0 0.0)
-;; 		 (-0.866025388240814 1.06054024092951e-16 0.5)
-;; 		 (0.866025388240814 0.0 0.5) (0.0 0.0 1.0))) 
-;;    (faces (quad-strip #u8(0 2 1 3 0 2)) (triangle-fan #u8(4 3 2 1 0))))))
-
-
-;; (mesh 
-;;  (vertices #2f32((-1.0 1.22460635382238e-16 0.0) 
-;; 		 (1.0 0.0 0.0) 
-;; 		 (-0.866025388240814 1.06054024092951e-16 0.5) 
-;; 		 (0.866025388240814 0.0 0.5) 
-;; 		 (0.0 0.0 1.0))) 
-;;  (colors #2f32((-1.0 1.22460635382238e-16 0.0) 
-;; 	       (1.0 0.0 0.0) 
-;; 	       (-0.866025388240814 1.06054024092951e-16 0.5) 
-;; 	       (0.866025388240814 0.0 0.5) 
-;; 	       (0.0 0.0 1.0))) 
-;;  (faces 
-;;   (quad-strip #u8(0 2 1 3 0 2)) 
-;;   (triangle-fan #u8(4 3 2 1 0))))
 
 (define-syntax define-symmetric-method
   (syntax-rules ()
@@ -240,7 +75,7 @@
   (light #:init-thunk 
 	 (lambda ()
 	   (make-light 
-	    #:ambient #f32(0 0 0 1)
+	    #:ambient #f32(.3 .3 .3 1)
 	    #:specular #f32(0 0 0 1)
 	    #:diffuse #f32(1 1 1 1)))))
 
@@ -269,11 +104,7 @@
 	 #:slot-set! (set-original! 'light)))
 
 (define-class <3d-object> (<3d>)
-  (mesh #:init-value '(mesh 
-		       (vertices #2f32((0 0 0)(1 0 0)(0 1 0)(0 0 1)))
-		       (colors #2f32((1 1 1)(1 0 0)(0 1 0)(0 0 1)))
-		       (faces
-			(lines #2u8((0 1)(0 2)(0 3)))))
+  (mesh #:init-value coordinate-system-mesh
 	#:init-keyword #:mesh))
 
 (define-class <3d-model> (<3d-object>)
@@ -286,8 +117,7 @@
 		      (let ((lights (extract-lights mesh))
 			    (mesh (skip-lights mesh)))
 			(set! #[this '%mesh] mesh)
-			(set! #[this '%lights] lights)))
-	#;(\ with-input-from-file "3d/cube.3d" read)))
+			(set! #[this '%lights] lights)))))
 
 (define-method (initialize (this <3d-model>) args)
   (next-method)
@@ -325,8 +155,7 @@
   (define-method (skip-lights (mesh <list>))
     (mesh-extractor skip-lights mesh
       (((? (lambda(x)(not (equal? x 'light))) command) . properties)
-       `(,command ,@properties))))
-  )
+       `(,command ,@properties)))))
 
 (define (transform-matrix! transform-spec)
   (for-each (match-lambda
@@ -428,6 +257,7 @@
   (translate-view! #[model 'position])
   (rotate-view! #[model 'orientation])
   (draw-mesh! #[model '%mesh])
+  ;;(draw-mesh! coordinate-system-mesh)
   (pop-matrix!))
 
 (define-method (draw-contour! (model <3d-object>))
