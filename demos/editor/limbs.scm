ced457d8be032bab9476ccc2aab3758104bd0906
Maciek Godek <godek.maciek@gmail.com>
2015-01-26 18:43:05 +0100

    Adding part-of-limb? function

--- demos/editor/limbs.scm
+++ demos/editor/limbs.scm
@@ -2,30 +2,52 @@
   #:use-module (scum physics)
   #:use-module (editor relations)
   #:use-module (extra math)
-  #:use-module (extra common))
+  #:use-module (extra common)
+  #:use-module (extra ref)
+  #:export (
+	    tip?
+	    link?
+	    copula?
+	    hub?
+	    body-part-type
+	    part-of-limb?
+	    part-of-corpus?
+	    ))
 
-(define (tip? limb)
-  (= 1 (length (joints-attached-to limb))))
+(define (tip? body-part)
+  (= 1 (length (joints-attached-to body-part))))
 
-(define (link? limb)
-  (= 2 (length (joints-attached-to limb))))
+(define (link? body-part)
+  (= 2 (length (joints-attached-to body-part))))
 
 (without-default (joint-property-getter)
-  (define (copula? limb)
-    (and-let* (((first-joint second-joint) (joints-attached-to limb)))
-      (< (square ((specific joint-property-getter) first-joint 'anchor)
-		 ((specific joint-property-getter) second-joint 'anchor))
+  (define (copula? body-part)
+    (and-let* (((first-joint second-joint) (joints-attached-to body-part)))
+      (< (square (- ((specific joint-property-getter) first-joint 'anchor)
+		    ((specific joint-property-getter) second-joint 'anchor)))
 	 #[TOLERANCE]))))
 
-(define (hub? limb)
-  (< 2 (length (joints-attached-to limb))))
+(define (hub? body-part)
+  (< 2 (length (joints-attached-to body-part))))
 
-(define (limb-type limb)
-  (cond ((tip? limb)
+(define (body-part-type body-part)
+  (cond ((tip? body-part)
 	 'tip)
-	((copula? limb)
+	((copula? body-part)
 	 'copula)
-	((link? limb)
+	((link? body-part)
 	 'link)
-	((hub? limb)
+	((hub? body-part)
 	 'hub)))
+
+(define (part-of-limb? body-part #:except '())
+  (or (tip? body-part)
+      (and-let* (((left right) (bodies-attached-to body-part))
+		 (exclusion `(,body-part ,@except)))
+	(or (and (not (in? left except))
+		 (part-of-limb? left #:except exclusion))
+	    (and (not (in? right except))
+		 (part-of-limb? right #:except exclusion))))))
+
+(define (part-of-corpus? body-part)
+  (not (part-of-limb? body-part)))
