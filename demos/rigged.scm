704e889e4f029abf7ca577cbe3ed2f7b6f3df5ff
Maciek Godek <godek.maciek@gmail.com>
2015-02-12 14:42:11 +0100

    Removed dependency on config.scm file

--- demos/rigged.scm
+++ demos/rigged.scm
@@ -414,4 +414,22 @@ exit
 	     (set! #[joint 'name] name)
 	     (add-object! joint #;to the-rig)))))))
 
-(load "config.scm")
+(let ((camera #[view 'camera]))
+  (set! #[camera 'position] #f32(0 -6 -0.7))
+  (set! #[camera 'orientation] (normalized '(1.0 . #f32(1 0 0)))))
+
+(key 'q (lambda () (relative-twist! #[view 'camera] #f32(0 0 0.02))))
+(key 'e (lambda () (relative-twist! #[view 'camera] #f32(0 0 -0.02))))
+(key 'w (lambda () (relative-move! #[view 'camera] #f32(0 0 -0.07))))
+(key 's (lambda () (relative-move! #[view 'camera] #f32(0 0 0.07))))
+(key 'a (lambda () (relative-move! #[view 'camera] #f32(-0.07 0 0))))
+(key 'd (lambda () (relative-move! #[view 'camera] #f32(0.07 0 0))))
+(key 'r (lambda () (relative-move! #[view 'camera] #f32(0 0.07 0))))
+(key 'f (lambda () (relative-move! #[view 'camera] #f32(0 -0.07 0))))
+(key 'up (lambda () (relative-turn! #[view 'camera] 0 2)))
+(key 'down (lambda () (relative-turn! #[view 'camera] 0 -2)))
+(key 'left (lambda () (relative-turn! #[view 'camera] 2 0)))
+(key 'right (lambda () (relative-turn! #[view 'camera] -2 0)))
+
+(set! #[view 'drag] (lambda (x y dx dy)
+		      (relative-turn! #[view 'camera] (- dx) (- dy))))
