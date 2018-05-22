00c7ce1cfab2658958dfe0862814d790a1b7df7d
Maciek Godek <godek.maciek@gmail.com>
2018-05-22 21:25:52 +0200

    * replaced dependence on (extra common) with (grand scheme) * (hopefully) improved the implementation of the "key" function

--- guile-modules/extra/slayer.scm
+++ guile-modules/extra/slayer.scm
@@ -1,10 +1,12 @@
 (define-module (extra slayer)
   #:use-module (slayer)
   #:use-module (slayer image)
-  #:use-module (extra common)
+  ;;#:use-module (extra common)
+  #:use-module (grand scheme)
+  #:use-module (extra array)
   #:use-module (extra ref)
   #:export (rgba highlighted subtract-image force-redisplay! key
-		 shift? ctrl? alt?)
+		 shift? ctrl? alt? add-mode! remove-mode!)
   #:export-syntax (with-video-output-to key-bindings)
   #:replace (
 	     (keydn-replacement . keydn)
@@ -17,15 +19,15 @@
     ((r g b a)
      (compose-color-from-rgba r g b a))))
 
-(define* (highlighted image #:key (red 0) (green 0) (blue 0))
+(define* (highlighted image red #:= 0 green #:= 0 blue #:= 0)
   (array->image
    (array-map (lambda(pixel)
-		(match-let (((r g b a) (rgba pixel))
-			    (+ (lambda (x y) (min 255 (+ x y)))))
+		(let ((`(,r ,g ,b ,a) (rgba pixel))
+		      (+ (lambda (x y) (min 255 (+ x y)))))
 		  (rgba (+ r red) (+ g green) (+ b blue) a)))
 	      (image->array image))))
 
-(define* (subtract-image subtrahend minuend #:key (tolerance 100))
+(define* (subtract-image subtrahend minuend tolerance #:= 100)
   (array->image
    (array-map (lambda (x y)
 		(let ((xrgba (rgba x))
@@ -51,21 +53,31 @@
       bindings ...
       fresh-bindings)))
 
-(define modes #[])
+(define *modes* #[])
+
+(define (add-mode! source handler)
+  (hash-set! *modes* source handler))
+
+(define (remove-mode! source)
+  (hash-remove! *modes* source))
 
 (add-timer! 
  30 #;ms
  (lambda()
-   (for (key => proc) in modes
+   (for (key => proc) in *modes*
      (proc))))
 
 (define (key name fun)
-  (keydn name 
-    (lambda()
-      (hash-set! modes name fun)))
-  (keyup name 
-    (lambda()
-      (hash-remove! modes name))))
+  (keydn name
+	 (with-procedure-properties
+	     ((source `(lambda () (add-mode! ',name ,(name/source fun)))))
+	   (lambda ()
+	     (add-mode! name fun))))
+  (keyup name
+	 (with-procedure-properties
+	     ((source `(lambda () (remove-mode! ',name))))
+	   (lambda()
+	     (remove-mode! name)))))
 
 (define (shift?)
   (modifier-pressed? 'shift))
@@ -103,9 +115,11 @@
 	  (set! old-bindings (current-key-bindings))
 	  (set-key-bindings! #[modifiers modifier]))))))
 
-(define (keydn-replacement key action)
-  (match key
-    ((_ . _)
-     (keydn* key action))
-    (_
-     (keydn key action))))
+(define (keydn-replacement . key+action)
+  (match key+action
+    (`((,modifier ,target) ,action)
+     (keydn* `(,modifier ,target) action))
+    (`(,key ,action)
+     (keydn key action))
+    (`(,key)
+     (keydn key))))
